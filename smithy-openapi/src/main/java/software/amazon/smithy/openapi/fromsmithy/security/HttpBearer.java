507cff8354eb24065e755a1762b3158ea30f714c
Michael Dowling <mtdowling@gmail.com>
2019-03-26 13:53:28 -0700

    Add Cognito User Pools support and more authn info
    This commit adds Cognito User Pool support to the OpenAPI converter and
    provides more informaiton to authentication scheme converters to make
    them easier to implement.

--- smithy-openapi/src/main/java/software/amazon/smithy/openapi/fromsmithy/security/HttpBearer.java
+++ smithy-openapi/src/main/java/software/amazon/smithy/openapi/fromsmithy/security/HttpBearer.java
@@ -1,5 +1,6 @@
 package software.amazon.smithy.openapi.fromsmithy.security;
 
+import software.amazon.smithy.model.traits.AuthenticationTrait;
 import software.amazon.smithy.openapi.fromsmithy.Context;
 import software.amazon.smithy.openapi.fromsmithy.SecuritySchemeConverter;
 import software.amazon.smithy.openapi.model.SecurityScheme;
@@ -14,7 +15,11 @@ public final class HttpBearer implements SecuritySchemeConverter {
     }
 
     @Override
-    public SecurityScheme createSecurityScheme(Context context) {
+    public SecurityScheme createSecurityScheme(
+            Context context,
+            AuthenticationTrait authTrait,
+            AuthenticationTrait.AuthScheme authScheme
+    ) {
         return SecurityScheme.builder()
                 .type("http")
                 .scheme("Bearer")
---
+++ smithy-openapi/src/test/java/software/amazon/smithy/openapi/fromsmithy/security/AmazonCognitoUserPoolsTest.java
@@ -0,0 +1,41 @@
+package software.amazon.smithy.openapi.fromsmithy.security;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.loader.LoaderUtils;
+import software.amazon.smithy.model.node.Node;
+import software.amazon.smithy.model.shapes.ShapeId;
+import software.amazon.smithy.openapi.OpenApiException;
+import software.amazon.smithy.openapi.fromsmithy.OpenApiConverter;
+
+public class AmazonCognitoUserPoolsTest {
+    @Test
+    public void addsAwsV4() {
+        Model model = Model.assembler()
+                .addImport(getClass().getResource("cognito-user-pools-security.json"))
+                .assemble()
+                .unwrap();
+        var result = OpenApiConverter.create().convert(model, ShapeId.from("smithy.example#Service"));
+        var expectedNode = Node.parse(LoaderUtils.readInputStream(
+                getClass().getResourceAsStream("cognito-user-pools-security.openapi.json"), "UTF-8"));
+
+        assertThat(result.toNode(), equalTo(expectedNode));
+    }
+
+    @Test
+    public void requiresProviderArns() {
+        var thrown = Assertions.assertThrows(OpenApiException.class, () -> {
+            Model model = Model.assembler()
+                    .addImport(getClass().getResource("invalid-cognito-user-pools-security.json"))
+                    .assemble()
+                    .unwrap();
+            OpenApiConverter.create().convert(model, ShapeId.from("smithy.example#Service"));
+        });
+
+        Assertions.assertTrue(thrown.getMessage().contains("providerARNs"));
+    }
+}
