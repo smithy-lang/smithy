62a905d169cbe5918b924beb5643ccc9bcd4dfb2
Michael Dowling <mtdowling@gmail.com>
2019-03-29 13:21:45 -0700

    Add ability to generically substitute Node values
    This commit adds the ability to generically substitute Node values using
    SmithyBuild. Literal values can be converted to a substitution value.
    This has been integrated into the Smithy2OpenApi build plugin to
    automatically handle explicitly declared replacements.

---
+++ smithy-build/src/test/java/software/amazon/smithy/build/JsonSubstitutionsTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.smithy.build;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import software.amazon.smithy.model.node.Node;
+import software.amazon.smithy.model.node.ObjectNode;
+
+public class JsonSubstitutionsTest {
+    @Test
+    public void replacesValuesInObjectNode() {
+        Map<String, Node> mappings = new HashMap<>();
+        mappings.put("FOO_BAR", Node.from("Hi!"));
+        JsonSubstitutions substitutions = JsonSubstitutions.create(mappings);
+        ObjectNode node = Node.parse("{\"a\": \"FOO_BAR\", \"b\": \"FOO_BAR baz\"}").expectObjectNode();
+        ObjectNode result = substitutions.apply(node).expectObjectNode();
+
+        assertThat(result.expectMember("a").expectStringNode().getValue(), equalTo("Hi!"));
+        assertThat(result.expectMember("b").expectStringNode().getValue(), equalTo("FOO_BAR baz"));
+    }
+
+    @Test
+    public void validatesKeysAdhereToPattern() {
+        Map<String, Node> mappings = new HashMap<>();
+        mappings.put("!FOO_BAR", Node.from("Hi!"));
+
+        Assertions.assertThrows(SmithyBuildException.class, () -> JsonSubstitutions.create(mappings));
+    }
+}
---
+++ smithy-openapi/src/test/java/software/amazon/smithy/openapi/fromsmithy/plugins/JsonSubstitutionsPluginTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.smithy.openapi.fromsmithy.plugins;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.node.Node;
+import software.amazon.smithy.model.shapes.ShapeId;
+import software.amazon.smithy.openapi.OpenApiConstants;
+import software.amazon.smithy.openapi.fromsmithy.OpenApiConverter;
+
+public class JsonSubstitutionsPluginTest {
+    @Test
+    public void removesBySubstitution() {
+        Model model = Model.assembler()
+                .addImport(RemoveUnusedComponentsTest.class.getResource("substitutions.smithy"))
+                .assemble()
+                .unwrap();
+
+        var openApi = OpenApiConverter.create()
+                .putSetting(OpenApiConstants.SUBSTITUTIONS, Node.objectNode()
+                        .withMember("SUB_HELLO", Node.from("hello")))
+                .convertToNode(model, ShapeId.from("smithy.example#Service"));
+        var description = openApi.getObjectMember("info").get().getStringMember("description").get().getValue();
+
+        Assertions.assertEquals("hello", description);
+    }
+}
