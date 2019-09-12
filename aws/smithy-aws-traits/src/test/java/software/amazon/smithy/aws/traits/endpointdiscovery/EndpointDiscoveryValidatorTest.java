dcf8fed08708e42fdfda92e2674c851150288d1f
JordonPhillips <phjordon@amazon.com>
2019-09-12 15:16:55 -0700

    Add endpoint discovery validator tests

---
+++ aws/smithy-aws-traits/src/test/java/software/amazon/smithy/aws/traits/endpointdiscovery/EndpointDiscoveryValidatorTest.java
@@ -0,0 +1,51 @@
+package software.amazon.smithy.aws.traits.endpointdiscovery;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.not;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.validation.Severity;
+import software.amazon.smithy.model.validation.ValidatedResult;
+import software.amazon.smithy.model.validation.ValidationEvent;
+
+public class EndpointDiscoveryValidatorTest {
+    @Test
+    public void validatesEndpointOperationBound() {
+        ValidatedResult<Model> result = Model.assembler()
+                .discoverModels(getClass().getClassLoader())
+                .addImport(getClass().getResource("endpoint-operation-unbound.json"))
+                .assemble();
+
+        List<ValidationEvent> events =  result.getValidationEvents(Severity.ERROR);
+        assertThat(events, not(empty()));
+        assertThat(events.get(0).getMessage(), containsString("must be bound"));
+    }
+
+    @Test
+    public void warnsOnConfiguredServicesWithoutBoundConfiguredOperations() {
+        ValidatedResult<Model> result = Model.assembler()
+                .discoverModels(getClass().getClassLoader())
+                .addImport(getClass().getResource("no-configured-operations.json"))
+                .assemble();
+
+        List<ValidationEvent> events =  result.getValidationEvents(Severity.WARNING);
+        assertThat(events, not(empty()));
+        assertThat(events.get(0).getMessage(), containsString("no operations bound"));
+    }
+
+    @Test
+    public void validatesOperationBoundToConfiguredService() {
+        ValidatedResult<Model> result = Model.assembler()
+                .discoverModels(getClass().getClassLoader())
+                .addImport(getClass().getResource("service-not-configured.json"))
+                .assemble();
+
+        List<ValidationEvent> events =  result.getValidationEvents(Severity.ERROR);
+        assertThat(events, not(empty()));
+        assertThat(events.get(0).getMessage(), containsString("not attached to a service"));
+    }
+}
