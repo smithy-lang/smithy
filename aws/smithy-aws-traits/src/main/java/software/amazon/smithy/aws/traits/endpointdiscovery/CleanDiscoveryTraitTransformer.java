76231691831dfa718033a08fb6d6256415e4e56b
JordonPhillips <phjordon@amazon.com>
2019-09-16 11:11:49 -0700

    fix checkstyle errors

--- aws/smithy-aws-traits/src/main/java/software/amazon/smithy/aws/traits/endpointdiscovery/CleanDiscoveryTraitTransformer.java
+++ aws/smithy-aws-traits/src/main/java/software/amazon/smithy/aws/traits/endpointdiscovery/CleanDiscoveryTraitTransformer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package software.amazon.smithy.aws.traits.endpointdiscovery;
 
 import java.util.Collection;
@@ -12,6 +27,9 @@ import software.amazon.smithy.model.traits.Trait;
 import software.amazon.smithy.model.transform.ModelTransformer;
 import software.amazon.smithy.model.transform.ModelTransformerPlugin;
 
+/**
+ * Removes the endpoint discovery trait from a service if the referenced operation or error are removed.
+ */
 public class CleanDiscoveryTraitTransformer implements ModelTransformerPlugin {
     @Override
     public Model onRemove(ModelTransformer transformer, Collection<Shape> shapes, Model model) {
--- aws/smithy-aws-traits/src/main/java/software/amazon/smithy/aws/traits/endpointdiscovery/EndpointDiscoveryTrait.java
+++ aws/smithy-aws-traits/src/main/java/software/amazon/smithy/aws/traits/endpointdiscovery/EndpointDiscoveryTrait.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package software.amazon.smithy.aws.traits.endpointdiscovery;
 
 import java.util.Arrays;
@@ -76,7 +91,7 @@ public final class EndpointDiscoveryTrait extends AbstractTrait implements ToSmi
                 .error(getError());
     }
 
-    /** Builder for {@link EndpointDiscoveryTrait} */
+    /** Builder for {@link EndpointDiscoveryTrait}. */
     public static final class Builder extends AbstractTraitBuilder<EndpointDiscoveryTrait, Builder> {
         private ShapeId operation;
         private ShapeId error;
