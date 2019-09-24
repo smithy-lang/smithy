bfd344183716610aedb6f3836612e5553bcef9d7
Michael Dowling <mtdowling@gmail.com>
2019-09-24 16:38:59 -0700

    Add a shape ID shader to help with codegen
    This abstraction can be used when generating code so that code is
    generated into a consolidated namespace rather than spread across
    all of the namespaces used in a Smithy model. `SymbolProvider`
    implementations can use the `ShapeIdShader` to handle renaming
    shape IDs before they convert the shape ID into a symbol appropriate
    for that language. This abstraction essentially normalizes shape IDs
    before the ID is converted into a language-specific symbol.
    
    For example, if you want to generate code into the "EC2" namespace,
    could set the `targetNamespace` to "EC2". If the service shape
    that is being generated is in the "com.amazon.ec2" namespace, then you
    could set the `rootNamespace` to "com.amazon.ec2" to completely
    elide that namespace so that "com.amazon.ec2" is mapped to "EC2".
    
    Any namespaces underneath the root namespace are then combined
    with the target namespace. By default, they are just merged
    (using APPEND). For example, given "com.amazon.ec2.nested.ns#Hello", the
    resulting shape ID is "EC2.nested.ns#Hello". The merge behavior can be
    customized by passing a `ShadeOption` to the `#shade(ShapeId, ShadeOption)`
    method.
    
    A shade option of `ShadeOption#SQUASH_INTO_NAME` combines
    all trailing namespaces into the shaded shape ID's name. For example,
    given "com.amazon.nested.ns#Hello", the shaded shape ID will become
    "EC2#NestedNsHello". The first character of each namespace segment is
    capitalized and prefixed on the original shape ID name.
    
    A shade option of `ShadeOption#SQUASH_INTO_NAMESPACE` combines
    all trailing namespaces into the shaded shape ID's namespace. For example,
    given "com.amazon.nested.ns#Hello", the shaded shape ID will become
    "EC2.NestedNs#Hello". The first character of each namespace segment is
    capitalized and appended as additional segments after the target namespace.
    
    Namespaces that are not inside of the given `rootNamespace`
    are concatenated with the `targetNamespace` and then merged like
    a sub-namespace. For example, given a "example.foo#Hi", the resulting
    shape ID is "EC2.example.foo#Hi". When using `SQUASH_INTO_NAME`,
    the resulting namespace is "EC2#ExampleFooHi". When using
    `SQUASH_INTO_NAMESPACE`, the resulting namespace is
    "EC2.ExampleFoo#Hi".

---
+++ codegen/smithy-codegen-core/src/test/java/software/amazon/smithy/codegen/core/ShapeIdShaderTest.java
@@ -0,0 +1,95 @@
+package software.amazon.smithy.codegen.core;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+import software.amazon.smithy.model.node.Node;
+import software.amazon.smithy.model.shapes.ShapeId;
+
+public class ShapeIdShaderTest {
+    @Test
+    public void removesRootFromTargetNamesapce() {
+        ShapeIdShader shader = ShapeIdShader.builder()
+                .targetNamespace("EC2")
+                .rootNamespace("com.amazon.ec2")
+                .build();
+
+        assertThat(shader.shade(ShapeId.from("com.amazon.ec2#Thing")),
+                   equalTo(ShapeId.from("EC2#Thing")));
+        assertThat(shader.shade(ShapeId.from("com.amazon.ec2.nested#Thing")),
+                   equalTo(ShapeId.from("EC2.nested#Thing")));
+        assertThat(shader.shade(ShapeId.from("not.same#Thing")),
+                   equalTo(ShapeId.from("EC2.not.same#Thing")));
+    }
+
+    @Test
+    public void squishesNamespacesIntoNames() {
+        ShapeIdShader shader = ShapeIdShader.builder()
+                .targetNamespace("EC2")
+                .rootNamespace("com.amazon.ec2")
+                .build();
+
+        assertThat(shader.shade(ShapeId.from("com.amazon.ec2#Thing"),
+                                ShapeIdShader.ShadeOption.SQUASH_INTO_NAME),
+                   equalTo(ShapeId.from("EC2#Thing")));
+        assertThat(shader.shade(ShapeId.from("com.amazon.ec2.nested#Thing"),
+                                ShapeIdShader.ShadeOption.SQUASH_INTO_NAME),
+                   equalTo(ShapeId.from("EC2#NestedThing")));
+        assertThat(shader.shade(ShapeId.from("not.same#Thing"), ShapeIdShader.ShadeOption.SQUASH_INTO_NAME),
+                   equalTo(ShapeId.from("EC2#NotSameThing")));
+    }
+
+    @Test
+    public void squishesNamespacesIntoNamespace() {
+        ShapeIdShader shader = ShapeIdShader.builder()
+                .targetNamespace("EC2")
+                .rootNamespace("com.amazon.ec2")
+                .build();
+
+        assertThat(shader.shade(ShapeId.from("com.amazon.ec2#Thing"),
+                                ShapeIdShader.ShadeOption.SQUASH_INTO_NAMESPACE),
+                   equalTo(ShapeId.from("EC2#Thing")));
+        assertThat(shader.shade(ShapeId.from("com.amazon.ec2.nested#Thing"),
+                                ShapeIdShader.ShadeOption.SQUASH_INTO_NAMESPACE),
+                   equalTo(ShapeId.from("EC2.Nested#Thing")));
+        assertThat(shader.shade(ShapeId.from("not.same#Thing"), ShapeIdShader.ShadeOption.SQUASH_INTO_NAMESPACE),
+                   equalTo(ShapeId.from("EC2.NotSame#Thing")));
+    }
+
+    @Test
+    public void returnsIdsAsIs() {
+        ShapeIdShader shader = ShapeIdShader.builder()
+                .targetNamespace("EC2")
+                .rootNamespace("EC2")
+                .build();
+
+        assertThat(shader.shade(ShapeId.from("EC2#Thing"),
+                                ShapeIdShader.ShadeOption.SQUASH_INTO_NAME),
+                   equalTo(ShapeId.from("EC2#Thing")));
+        assertThat(shader.shade(ShapeId.from("EC2#Thing"),
+                                ShapeIdShader.ShadeOption.SQUASH_INTO_NAMESPACE),
+                   equalTo(ShapeId.from("EC2#Thing")));
+        assertThat(shader.shade(ShapeId.from("EC2#Thing")), equalTo(ShapeId.from("EC2#Thing")));
+    }
+
+    @Test
+    public void sanitizesShapeIdTarget() {
+        ShapeIdShader shader = ShapeIdShader.builder()
+                .targetNamespace("Hey...you... guys!")
+                .rootNamespace("com.amazon.example")
+                .build();
+
+        assertThat(shader.shade(ShapeId.from("com.amazon.example#BabyRuth")),
+                   equalTo(ShapeId.from("Hey.you._guys_#BabyRuth")));
+    }
+
+    @Test
+    public void canLoadFromConfig() {
+        ShapeIdShader shader = ShapeIdShader.builder()
+                .fromNode(Node.parse("{\"rootNamespace\": \"com.foo\", \"targetNamespace\": \"Foo\"}"))
+                .build();
+
+        assertThat(shader.shade(ShapeId.from("com.foo.bar#Hi")), equalTo(ShapeId.from("Foo.bar#Hi")));
+    }
+}
