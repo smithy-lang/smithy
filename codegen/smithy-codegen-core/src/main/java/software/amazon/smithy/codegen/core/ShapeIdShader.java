6b3e24c89db7dd441c1336abe5f26fa9a70576c7
Michael Dowling <mtdowling@gmail.com>
2019-09-29 15:06:33 -0700

    Simplify ShapeIdShader as an enum

--- codegen/smithy-codegen-core/src/main/java/software/amazon/smithy/codegen/core/ShapeIdShader.java
+++ codegen/smithy-codegen-core/src/main/java/software/amazon/smithy/codegen/core/ShapeIdShader.java
@@ -15,13 +15,10 @@
 
 package software.amazon.smithy.codegen.core;
 
+import java.util.function.Function;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
-import software.amazon.smithy.model.node.Node;
-import software.amazon.smithy.model.node.ObjectNode;
 import software.amazon.smithy.model.shapes.ShapeId;
-import software.amazon.smithy.utils.SmithyBuilder;
-import software.amazon.smithy.utils.StringUtils;
 
 /**
  * Shades and renames shape IDs into a target namespace.
@@ -40,130 +37,111 @@ import software.amazon.smithy.utils.StringUtils;
  * could set the {@code rootNamespace} to "com.amazon.ec2" to completely
  * elide that namespace so that "com.amazon.ec2" is mapped to "EC2".
  *
- * <p>Any namespaces underneath the root namespace are then combined
- * with the target namespace. By default, they are just merged using
- * {@link ShadeOption#APPEND}. For example, given
- * "com.amazon.ec2.nested.ns#Hello", the resulting shape ID is
- * "EC2.nested.ns#Hello". The merge behavior can be customized by passing
- * a {@link ShadeOption} to the {@link #shade(ShapeId, ShadeOption)} method.
+ * {@link #APPEND} appends namespaces outside of the root to the target.
+ * Namespaces that are not inside of the given {@code rootNamespace}
+ * are concatenated with the {@code targetNamespace} and then merged like
+ * a sub-namespace. For example, given a "example.foo#Hi", the resulting
+ * shape ID is "EC2.example.foo#Hi".
  *
- * <p>A shade option of {@link ShadeOption#MERGE_NAME} combines
- * all trailing namespaces into the shaded shape ID's name. For example,
- * given "com.amazon.nested.ns#Hello", the shaded shape ID will become
- * "EC2#NestedNsHello". The first character of each namespace segment is
- * capitalized and prefixed on the original shape ID name.
+ * <p>{@link #MERGE_NAME} combines all trailing namespaces into the shaded
+ * shape ID's name. For example, given "com.amazon.nested.ns#Hello", the
+ * shaded shape ID will become "EC2#NestedNsHello". The first character
+ * of each namespace segment is capitalized and prefixed on the original
+ * shape ID name.
  *
- * <p>A shade option of {@link ShadeOption#MERGE_NAMESPACE} combines
- * all trailing namespaces into the shaded shape ID's namespace. For example,
- * given "com.amazon.nested.ns#Hello", the shaded shape ID will become
+ * <p>{@link #MERGE_NAMESPACE} combines all trailing namespaces into the
+ * shaded shape ID's namespace. For example, given
+ * "com.amazon.nested.ns#Hello", the shaded shape ID will become
  * "EC2.NestedNs#Hello". The first character of each namespace segment is
  * capitalized and appended as additional segments after the target namespace.
  *
- * <p>Namespaces that are not inside of the given {@code rootNamespace}
- * are concatenated with the {@code targetNamespace} and then merged like
- * a sub-namespace. For example, given a "example.foo#Hi", the resulting
- * shape ID is "EC2.example.foo#Hi". When using {@code MERGE_NAME},
- * the resulting namespace is "EC2#ExampleFooHi". When using
- * {@code MERGE_NAMESPACE}, the resulting namespace is
- * "EC2.ExampleFoo#Hi".
+ * <p>{@link #FLATTEN} ignores given namespaces and changes them to the target
+ * namespace. Warning: this runs a high risk of shape name conflicts across
+ * namespaces! For example, given "com.amazon.nested.ns#Hello", the shaded
+ * shape ID will become "EC2#Hello".
  */
-public final class ShapeIdShader {
+public enum ShapeIdShader {
+    /**
+     * Appends additional namespace segments outside of the root namespace
+     * to the end of the target namespace.
+     *
+     * <p>For example, given a target of "Foo", a root of "foo.bar", and an ID of
+     * of "com.example#Baz", the resulting namespace of "Foo.com.example#Baz".
+     */
+    APPEND {
+        @Override
+        ShapeId applyShade(ShapeId id, String targetNamespace, String shadedNamespace) {
+            shadedNamespace = targetNamespace + "." + shadedNamespace;
+            return ShapeId.fromParts(shadedNamespace, id.getName(), id.getMember().orElse(null));
+        }
+    },
 
-    public static final String ROOT_NAMESPACE = "rootNamespace";
-    public static final String TARGET_NAMESPACE = "targetNamespace";
+    /**
+     * Merges additional namespace segments outside of the root namespace into
+     * the name of the shape.
+     *
+     * <p>For example, given a target of "Foo", a root of "foo.bar", and an ID of
+     * of "com.example#Baz", the resulting namespace of "Foo#ComExampleBaz". The
+     * first character of the generated shape name is always capitalized.
+     */
+    MERGE_NAME {
+        @Override
+        ShapeId applyShade(ShapeId id, String targetNamespace, String shadedNamespace) {
+            String name = mergeName(shadedNamespace + "." + id.getName());
+            return ShapeId.fromParts(targetNamespace, name, id.getMember().orElse(null));
+        }
+    },
+
+    /**
+     * Merges additional namespace segments outside of the root namespace into
+     * a single second-level namespace.
+     *
+     * <p>For example, given a target of "Foo", a root of "foo.bar", and a
+     * namespace of "com.example", the resulting namespace of "Foo.ComExample".
+     * The generated namespace will capitalize the first character of every
+     * appended namespace segment.
+     */
+    MERGE_NAMESPACE {
+        @Override
+        ShapeId applyShade(ShapeId id, String targetNamespace, String shadedNamespace) {
+            shadedNamespace = targetNamespace + "." + mergeName(shadedNamespace);
+            return ShapeId.fromParts(shadedNamespace, id.getName(), id.getMember().orElse(null));
+        }
+    },
+
+    /**
+     * Flattens all namespaces into a single namespace.
+     *
+     * <p>This shader simply changes every namespace to the target namespace.
+     */
+    FLATTEN {
+        @Override
+        ShapeId applyShade(ShapeId id, String targetNamespace, String shadedNamespace) {
+            return ShapeId.fromParts(targetNamespace, id.getName(), id.getMember().orElse(null));
+        }
+    };
 
     private static final Logger LOGGER = Logger.getLogger(ShapeIdShader.class.getName());
-    private static final Pattern DOT = Pattern.compile("\\.");
     private static final Pattern SANITIZE_CHARS = Pattern.compile("([^A-Za-z0-9_.]+)");
-    private static final Pattern SANITIZE_DOTS = Pattern.compile("(\\.+)");
-
-    private final String rootNamespace;
-    private final String targetNamespace;
-
-    private ShapeIdShader(Builder builder) {
-        String target = SmithyBuilder.requiredState(TARGET_NAMESPACE, builder.targetNamespace);
-
-        if (!ShapeId.isValidNamespace(target)) {
-            target = SANITIZE_CHARS.matcher(target).replaceAll("_");
-            target = SANITIZE_DOTS.matcher(target).replaceAll(".");
-            LOGGER.info(String.format("Sanitized %s `%s` to `%s`", TARGET_NAMESPACE, builder.targetNamespace, target));
-        }
-
-        rootNamespace = SmithyBuilder.requiredState(ROOT_NAMESPACE, builder.rootNamespace);
-        targetNamespace = target;
-    }
+    private static final Pattern SANITIZE_DOTS = Pattern.compile("(\\.{2,})");
 
     /**
-     * Builds a new {@code ShapeIdShader}.
+     * Shades the given shape ID into the target namespace stripping the given root.
      *
-     * @return Returns the builder.
-     */
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    /**
-     * Creates a shaded version of the given shape ID.
-     *
-     * @param id Shape ID to shade.
+     * @param id The shape ID to shade.
+     * @param rootNamespace Namespace to use as the "root".
+     * @param targetNamespace Namespace to shade into.
      * @return Returns the shaded shape ID.
      */
-    public ShapeId shade(ShapeId id) {
-        return shade(id, ShadeOption.APPEND);
-    }
-
-    /**
-     * Creates a shaded version of the given shape ID and combines
-     * any additional namespace segments using the provided
-     * {@code ShadeOption} strategy.
-     *
-     * @param id Shape ID to shade.
-     * @param option The shade strategy to use.
-     * @return Returns the shaded shape ID.
-     */
-    public ShapeId shade(ShapeId id, ShadeOption option) {
-        String name = id.getName();
-        String namespace = shadeNamespace(id);
-
-        if (namespace.isEmpty()) {
-            namespace = targetNamespace;
-        } else {
-            switch (option) {
-                case APPEND:
-                    namespace = targetNamespace + "." + namespace;
-                    break;
-                case MERGE_NAME:
-                    name = squashName(namespace + "." + name);
-                    namespace = targetNamespace;
-                    break;
-                case MERGE_NAMESPACE:
-                    namespace = targetNamespace + "." + squashName(namespace);
-                    break;
-                default:
-                    throw new UnsupportedOperationException("Unreachable shade option: " + option);
-            }
-        }
-
-        if (namespace.equals(id.getNamespace()) && name.equals(id.getName())) {
-            return id;
-        }
-
-        return ShapeId.fromParts(namespace, name, id.getMember().orElse(null));
-    }
-
-    private String squashName(String name) {
-        StringBuilder builder = new StringBuilder();
-        for (String identifier : DOT.split(name)) {
-            builder.append(StringUtils.capitalize(identifier));
-        }
-        return builder.toString();
-    }
-
-    private String shadeNamespace(ShapeId id) {
+    public ShapeId shade(ShapeId id, String rootNamespace, String targetNamespace) {
         String namespace = id.getNamespace();
 
+        // If the namespace equals the root namespace, then just return the shape
+        // ID equivalent in the target namespace with the same name + member.
         if (namespace.equals(rootNamespace)) {
-            return "";
+            return targetNamespace.equals(rootNamespace)
+                   ? id
+                   : ShapeId.fromParts(targetNamespace, id.getName(), id.getMember().orElse(null));
         }
 
         if (namespace.startsWith(rootNamespace + ".")) {
@@ -171,101 +149,67 @@ public final class ShapeIdShader {
             namespace = namespace.substring(rootNamespace.length() + 1);
         }
 
-        return namespace;
+        return applyShade(id, targetNamespace, namespace);
+    }
+
+    abstract ShapeId applyShade(ShapeId id, String targetNamespace, String shadedNamespace);
+
+    /**
+     * Sanitizes a given string to be used as a namespace.
+     *
+     * <p>Note that this value represents a ShapeID namespace, not a
+     * namespace for a target programming language. The target namespace
+     * is sanitized so that it is compatible with a Smithy shape ID. Special
+     * characters that don't match the ShapeID syntax are replaced with "_".
+     * Multiple successive dots are replaced with a single dot.
+     *
+     * @param targetNamespace Shape ID to target.
+     * @return Returns the sanitized target namespace.
+     */
+    public static String sanitizeTargetNamespace(String targetNamespace) {
+        String updated = targetNamespace;
+        if (!ShapeId.isValidNamespace(targetNamespace)) {
+            updated = SANITIZE_CHARS.matcher(targetNamespace).replaceAll("_");
+            updated = SANITIZE_DOTS.matcher(updated).replaceAll(".");
+            LOGGER.info(String.format("Sanitized target namespace `%s` to `%s`", targetNamespace, updated));
+        }
+        return updated;
     }
 
     /**
-     * Controls how shading is handled when there are namespace segments found
-     * outside of the {@code rootNamespace}.
+     * Creates a function that is used to shade shape IDs with a specific strategy
+     * from a root namespace into a sanitized target namespace.
+     *
+     * <p>The provided {@code targetNamespace} is sanitized using
+     * {@link  #sanitizeTargetNamespace}.
+     *
+     * @param rootNamespace Root namespace that is trimmed from IDs.
+     * @param targetNamespace Target namespace to generate IDs into.
+     * @param shader Shade strategy to use.
+     * @return Returns the created shader function.
      */
-    public enum ShadeOption {
-        /**
-         * Appends additional namespace segments outside of the root namespace
-         * to the end of the target namespace.
-         *
-         * <p>For example, given a target of "Foo", a root of "foo.bar", and an ID of
-         * of "com.example#Baz", the resulting namespace of "Foo.com.example#Baz".
-         */
-        APPEND,
-
-        /**
-         * Merges additional namespace segments outside of the root namespace into
-         * the name of the shape.
-         *
-         * <p>For example, given a target of "Foo", a root of "foo.bar", and an ID of
-         * of "com.example#Baz", the resulting namespace of "Foo#ComExampleBaz".
-         */
-        MERGE_NAME,
-
-        /**
-         * Merges additional namespace segments outside of the root namespace into
-         * a single second-level namespace.
-         *
-         * <p>For example, given a target of "Foo", a root of "foo.bar", and a namespace
-         * of "com.example", the resulting namespace of "Foo.ComExample".
-         */
-        MERGE_NAMESPACE
+    public static Function<ShapeId, ShapeId> createShader(
+            String rootNamespace, String targetNamespace, ShapeIdShader shader) {
+        String sanitizeTargetNamespace = sanitizeTargetNamespace(targetNamespace);
+        return id -> shader.shade(id, rootNamespace, sanitizeTargetNamespace);
     }
 
-    /**
-     * Builds the shape ID shader.
-     */
-    public static final class Builder implements SmithyBuilder<ShapeIdShader> {
-        private String rootNamespace;
-        private String targetNamespace;
+    private static String mergeName(String name) {
+        StringBuilder builder = new StringBuilder();
+        boolean afterDot = true;
 
-        @Override
-        public ShapeIdShader build() {
-            return new ShapeIdShader(this);
-        }
-
-        /**
-         * Root namespace that is removed from the targeted namespace.
-         *
-         * @param rootNamespace Root namespace to set.
-         * @return Returns the builder.
-         */
-        public Builder rootNamespace(String rootNamespace) {
-            this.rootNamespace = rootNamespace;
-            return this;
-        }
-
-        /**
-         * Target namespace where all namespaces will be generated into.
-         *
-         * <p>Note that this value represents a ShapeID namespace, not a
-         * namespace for a target programming language. The target namespace
-         * is sanitized so that it is compatible with a Smithy shape ID.
-         * Special characters that don't match the ShapeID syntax are
-         * replaced with "_". Multiple successive dots are replaced
-         * with a single dot.
-         *
-         * @param targetNamespace Namespace to generate into.
-         * @return Returns the builder.
-         */
-        public Builder targetNamespace(String targetNamespace) {
-            this.targetNamespace = targetNamespace;
-            return this;
-        }
-
-        /**
-         * Loads configuration settings from a {@link Node} configuration.
-         *
-         * @param node Node to load settings from.
-         * @return Returns the builder.
-         */
-        public Builder fromNode(Node node) {
-            ObjectNode value = node.expectObjectNode();
-
-            if (value.getMember(ROOT_NAMESPACE).isPresent()) {
-                rootNamespace = value.expectStringMember(ROOT_NAMESPACE).getValue();
+        for (int i = 0; i < name.length(); i++) {
+            char c = name.charAt(i);
+            if (c == '.') {
+                afterDot = true;
+            } else if (afterDot) {
+                builder.append(Character.toUpperCase(c));
+                afterDot = false;
+            } else {
+                builder.append(c);
             }
-
-            if (value.getMember(TARGET_NAMESPACE).isPresent()) {
-                targetNamespace = value.expectStringMember(TARGET_NAMESPACE).getValue();
-            }
-
-            return this;
         }
+
+        return builder.toString();
     }
 }
--- codegen/smithy-codegen-core/src/test/java/software/amazon/smithy/codegen/core/ShapeIdShaderTest.java
+++ codegen/smithy-codegen-core/src/test/java/software/amazon/smithy/codegen/core/ShapeIdShaderTest.java
@@ -3,93 +3,69 @@ package software.amazon.smithy.codegen.core;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.util.function.Function;
 import org.junit.jupiter.api.Test;
-import software.amazon.smithy.model.node.Node;
 import software.amazon.smithy.model.shapes.ShapeId;
 
 public class ShapeIdShaderTest {
     @Test
     public void removesRootFromTargetNamesapce() {
-        ShapeIdShader shader = ShapeIdShader.builder()
-                .targetNamespace("EC2")
-                .rootNamespace("com.amazon.ec2")
-                .build();
+        Function<ShapeId, ShapeId> shader = ShapeIdShader.createShader(
+                "com.amazon.ec2", "EC2", ShapeIdShader.APPEND);
 
-        assertThat(shader.shade(ShapeId.from("com.amazon.ec2#Thing")),
+        assertThat(shader.apply(ShapeId.from("com.amazon.ec2#Thing")),
                    equalTo(ShapeId.from("EC2#Thing")));
-        assertThat(shader.shade(ShapeId.from("com.amazon.ec2.nested#Thing")),
-                   equalTo(ShapeId.from("EC2.nested#Thing")));
-        assertThat(shader.shade(ShapeId.from("not.same#Thing")),
+        assertThat(shader.apply(ShapeId.from("com.amazon.ec2.Nested#Thing")),
+                   equalTo(ShapeId.from("EC2.Nested#Thing")));
+        assertThat(shader.apply(ShapeId.from("not.same#Thing")),
                    equalTo(ShapeId.from("EC2.not.same#Thing")));
     }
 
     @Test
-    public void squishesNamespacesIntoNames() {
-        ShapeIdShader shader = ShapeIdShader.builder()
-                .targetNamespace("EC2")
-                .rootNamespace("com.amazon.ec2")
-                .build();
+    public void mergesNamespacesIntoNames() {
+        Function<ShapeId, ShapeId> shader = ShapeIdShader.createShader(
+                "com.amazon.ec2", "EC2", ShapeIdShader.MERGE_NAME);
 
-        assertThat(shader.shade(ShapeId.from("com.amazon.ec2#Thing"),
-                                ShapeIdShader.ShadeOption.MERGE_NAME),
-                   equalTo(ShapeId.from("EC2#Thing")));
-        assertThat(shader.shade(ShapeId.from("com.amazon.ec2.nested#Thing"),
-                                ShapeIdShader.ShadeOption.MERGE_NAME),
+        assertThat(shader.apply(ShapeId.from("com.amazon.ec2#Thing")), equalTo(ShapeId.from("EC2#Thing")));
+        assertThat(shader.apply(ShapeId.from("com.amazon.ec2.nested#Thing")),
                    equalTo(ShapeId.from("EC2#NestedThing")));
-        assertThat(shader.shade(ShapeId.from("not.same#Thing"), ShapeIdShader.ShadeOption.MERGE_NAME),
-                   equalTo(ShapeId.from("EC2#NotSameThing")));
+        assertThat(shader.apply(ShapeId.from("not.same#Thing")), equalTo(ShapeId.from("EC2#NotSameThing")));
     }
 
     @Test
-    public void squishesNamespacesIntoNamespace() {
-        ShapeIdShader shader = ShapeIdShader.builder()
-                .targetNamespace("EC2")
-                .rootNamespace("com.amazon.ec2")
-                .build();
+    public void mergesNamespacesIntoNamespace() {
+        Function<ShapeId, ShapeId> shader = ShapeIdShader.createShader(
+                "com.amazon.ec2", "EC2", ShapeIdShader.MERGE_NAMESPACE);
 
-        assertThat(shader.shade(ShapeId.from("com.amazon.ec2#Thing"),
-                                ShapeIdShader.ShadeOption.MERGE_NAMESPACE),
-                   equalTo(ShapeId.from("EC2#Thing")));
-        assertThat(shader.shade(ShapeId.from("com.amazon.ec2.nested#Thing"),
-                                ShapeIdShader.ShadeOption.MERGE_NAMESPACE),
+        assertThat(shader.apply(ShapeId.from("com.amazon.ec2#Thing")), equalTo(ShapeId.from("EC2#Thing")));
+        assertThat(shader.apply(ShapeId.from("com.amazon.ec2.Nested#Thing")),
                    equalTo(ShapeId.from("EC2.Nested#Thing")));
-        assertThat(shader.shade(ShapeId.from("not.same#Thing"), ShapeIdShader.ShadeOption.MERGE_NAMESPACE),
-                   equalTo(ShapeId.from("EC2.NotSame#Thing")));
+        assertThat(shader.apply(ShapeId.from("not.same#Thing")), equalTo(ShapeId.from("EC2.NotSame#Thing")));
+    }
+
+    @Test
+    public void flattensNamespaces() {
+        Function<ShapeId, ShapeId> shader = ShapeIdShader.createShader(
+                "com.amazon.ec2", "EC2", ShapeIdShader.FLATTEN);
+
+        assertThat(shader.apply(ShapeId.from("com.amazon.ec2#Thing")), equalTo(ShapeId.from("EC2#Thing")));
+        assertThat(shader.apply(ShapeId.from("com.amazon.ec2.Nested#Thing")), equalTo(ShapeId.from("EC2#Thing")));
+        assertThat(shader.apply(ShapeId.from("not.same#Thing")), equalTo(ShapeId.from("EC2#Thing")));
     }
 
     @Test
     public void returnsIdsAsIs() {
-        ShapeIdShader shader = ShapeIdShader.builder()
-                .targetNamespace("EC2")
-                .rootNamespace("EC2")
-                .build();
+        Function<ShapeId, ShapeId> shader = ShapeIdShader.createShader("EC2", "EC2", ShapeIdShader.MERGE_NAME);
 
-        assertThat(shader.shade(ShapeId.from("EC2#Thing"),
-                                ShapeIdShader.ShadeOption.MERGE_NAME),
-                   equalTo(ShapeId.from("EC2#Thing")));
-        assertThat(shader.shade(ShapeId.from("EC2#Thing"),
-                                ShapeIdShader.ShadeOption.MERGE_NAMESPACE),
-                   equalTo(ShapeId.from("EC2#Thing")));
-        assertThat(shader.shade(ShapeId.from("EC2#Thing")), equalTo(ShapeId.from("EC2#Thing")));
+        assertThat(shader.apply(ShapeId.from("EC2#Thing")), equalTo(ShapeId.from("EC2#Thing")));
     }
 
     @Test
     public void sanitizesShapeIdTarget() {
-        ShapeIdShader shader = ShapeIdShader.builder()
-                .targetNamespace("Hey...you... guys!")
-                .rootNamespace("com.amazon.example")
-                .build();
+        Function<ShapeId, ShapeId> shader = ShapeIdShader.createShader(
+                "com.amazon.example", "Hey...you... guys!", ShapeIdShader.APPEND);
 
-        assertThat(shader.shade(ShapeId.from("com.amazon.example#BabyRuth")),
+        assertThat(shader.apply(ShapeId.from("com.amazon.example#BabyRuth")),
                    equalTo(ShapeId.from("Hey.you._guys_#BabyRuth")));
-    }
-
-    @Test
-    public void canLoadFromConfig() {
-        ShapeIdShader shader = ShapeIdShader.builder()
-                .fromNode(Node.parse("{\"rootNamespace\": \"com.foo\", \"targetNamespace\": \"Foo\"}"))
-                .build();
-
-        assertThat(shader.shade(ShapeId.from("com.foo.bar#Hi")), equalTo(ShapeId.from("Foo.bar#Hi")));
     }
 }
