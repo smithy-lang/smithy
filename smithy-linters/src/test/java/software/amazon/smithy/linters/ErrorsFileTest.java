4c9d53dcd7c56d70a649fa8a4eedd1b5dd309a6c
Michael Dowling <mtdowling@gmail.com>
2020-02-04 15:52:44 -0800

    Remove no longer needed protocol linters

--- smithy-linters/src/test/java/software/amazon/smithy/linters/ErrorsFileTest.java
+++ smithy-linters/src/test/java/software/amazon/smithy/linters/ErrorsFileTest.java
@@ -15,7 +15,11 @@
 
 package software.amazon.smithy.linters;
 
-import org.junit.jupiter.api.Test;
+import java.util.concurrent.Callable;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import software.amazon.smithy.model.validation.testrunner.SmithyTestCase;
 import software.amazon.smithy.model.validation.testrunner.SmithyTestSuite;
 
 /**
@@ -23,10 +27,13 @@ import software.amazon.smithy.model.validation.testrunner.SmithyTestSuite;
  * that the corresponding .errors file errors match the validation events of loading the file.
  */
 public class ErrorsFileTest {
-    @Test
-    public void testRunner() {
-        System.out.println(SmithyTestSuite.runner()
-                .addTestCasesFromUrl(ErrorsFileTest.class.getResource("errorfiles"))
-                .run());
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("source")
+    public void testRunner(String filename, Callable<SmithyTestCase.Result> callable) throws Exception {
+        callable.call();
+    }
+
+    public static Stream<?> source() {
+        return SmithyTestSuite.defaultParameterizedTestSource(ErrorsFileTest.class);
     }
 }
