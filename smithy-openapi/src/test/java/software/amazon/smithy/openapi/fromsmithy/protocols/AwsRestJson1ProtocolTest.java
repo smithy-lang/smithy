cd8d84a5185f7ddb0d58bbb559c78a2b371694c0
Michael Dowling <mtdowling@gmail.com>
2020-02-13 14:59:35 -0800

    Use $ref for request/response OpenAPI contents
    We previously generated inline schemas for the contents (document
    bindings) of requests and responses. This resulted in randomly generated
    schemas in some tools like API Gateway. This change now adds the ability
    to "synthesize" shapes during the conversion so that we can now generate
    schemas for these cases.
    
    This change also detected and fixed a bug where the headers of an
    operations's response were being used when generating errors responses
    rather than the header bindings on the error.

--- smithy-openapi/src/test/java/software/amazon/smithy/openapi/fromsmithy/protocols/AwsRestJson1ProtocolTest.java
+++ smithy-openapi/src/test/java/software/amazon/smithy/openapi/fromsmithy/protocols/AwsRestJson1ProtocolTest.java
@@ -27,7 +27,8 @@ public class AwsRestJson1ProtocolTest {
             "adds-header-timestamp-format.json",
             "adds-header-mediatype-format.json",
             "supports-payloads.json",
-            "aws-rest-json-uses-jsonname.json"
+            "aws-rest-json-uses-jsonname.json",
+            "synthesizes-contents.json"
     })
 
     public void testProtocolResult(String smithy) {
