979fb3b6fb6b941400629bdb7cfdcd315c08371c
Chase Coalwell <chasecoa@amazon.com>
2020-02-20 14:02:53 -0800

    Emit warnings for shapes bound to deprecated traits (#279)

---
+++ smithy-model/src/main/java/software/amazon/smithy/model/validation/validators/DeprecatedTraitsValidator.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.smithy.model.validation.validators;
+
+import static java.lang.String.format;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.shapes.Shape;
+import software.amazon.smithy.model.shapes.ShapeId;
+import software.amazon.smithy.model.traits.DeprecatedTrait;
+import software.amazon.smithy.model.validation.AbstractValidator;
+import software.amazon.smithy.model.validation.ValidationEvent;
+
+/**
+ * Emits a validation event if a model contains shapes that are bound to deprecated traits.
+ */
+public final class DeprecatedTraitsValidator extends AbstractValidator {
+    @Override
+    public List<ValidationEvent> validate(Model model) {
+        Set<ShapeId> deprecatedTraits = model.getTraitShapes().stream()
+                .filter(trait -> trait.hasTrait(DeprecatedTrait.class))
+                .map(Shape::getId)
+                .collect(Collectors.toSet());
+
+        return model.shapes()
+                .flatMap(shape -> validateShape(model, shape, deprecatedTraits).stream())
+                .collect(Collectors.toList());
+    }
+
+    private List<ValidationEvent> validateShape(Model model, Shape shape, Set<ShapeId> deprecatedTraits) {
+        List<ValidationEvent> events = new ArrayList<>();
+        shape.getAllTraits().forEach((shapeId, trait) -> {
+            if (!deprecatedTraits.contains(trait.toShapeId())) {
+                return;
+            }
+            DeprecatedTrait deprecatedTrait = model.expectShape(trait.toShapeId()).expectTrait(DeprecatedTrait.class);
+            String traitMessage = trait.toShapeId().toString();
+            if (deprecatedTrait.getMessage().isPresent()) {
+                traitMessage = traitMessage + ", " + deprecatedTrait.getMessage().get();
+            }
+            events.add(warning(shape, trait, format("This shape applies a trait that is deprecated: %s",
+                    traitMessage)));
+        });
+        return events;
+    }
+}
