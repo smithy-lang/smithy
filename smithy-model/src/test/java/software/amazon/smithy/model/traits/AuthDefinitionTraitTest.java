ebe6b54c0fcfd0e34461eae8e5fb7b7e64d7d1c6
Michael Dowling <mtdowling@gmail.com>
2020-02-24 11:19:12 -0800

    Add trait requirements to auth scheme too

---
+++ smithy-model/src/test/java/software/amazon/smithy/model/traits/AuthDefinitionTraitTest.java
@@ -0,0 +1,36 @@
+package software.amazon.smithy.model.traits;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import software.amazon.smithy.model.node.ArrayNode;
+import software.amazon.smithy.model.node.Node;
+import software.amazon.smithy.model.shapes.ShapeId;
+
+public class AuthDefinitionTraitTest {
+    @Test
+    public void loadsTrait() {
+        TraitFactory provider = TraitFactory.createServiceFactory();
+        ArrayNode values = Node.fromStrings(
+                JsonNameTrait.ID.toString(),
+                XmlNameTrait.ID.toString());
+        Node node = Node.objectNode().withMember("traits", values);
+        Optional<Trait> trait = provider.createTrait(
+                ShapeId.from("smithy.api#authDefinition"),
+                ShapeId.from("ns.qux#foo"),
+                node);
+
+        assertTrue(trait.isPresent());
+        assertThat(trait.get(), instanceOf(AuthDefinitionTrait.class));
+        AuthDefinitionTrait authDefinitionTrait = (AuthDefinitionTrait) trait.get();
+        assertThat(authDefinitionTrait.getTraits(), containsInAnyOrder(
+                JsonNameTrait.ID, XmlNameTrait.ID));
+        assertThat(authDefinitionTrait.toNode(), equalTo(node));
+        assertThat(authDefinitionTrait.toBuilder().build(), equalTo(authDefinitionTrait));
+    }
+}
