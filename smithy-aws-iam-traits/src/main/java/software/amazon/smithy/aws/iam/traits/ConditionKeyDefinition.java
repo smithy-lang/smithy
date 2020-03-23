8fc0486f81d6bd3ba53871f9402211f695426798
Michael Dowling <mtdowling@gmail.com>
2020-03-23 15:05:53 -0700

    Move IAM traits to its own package
    This change allows us to evolve the IAM traits independent of the rest
    of the AWS traits. We shouldn't put *all* AWS traits into a single
    package because it requires that the traits are all changed in unison.

--- smithy-aws-iam-traits/src/main/java/software/amazon/smithy/aws/iam/traits/ConditionKeyDefinition.java
+++ smithy-aws-iam-traits/src/main/java/software/amazon/smithy/aws/iam/traits/ConditionKeyDefinition.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * permissions and limitations under the License.
  */
 
-package software.amazon.smithy.aws.traits.iam;
+package software.amazon.smithy.aws.iam.traits;
 
 import java.util.List;
 import java.util.Objects;
