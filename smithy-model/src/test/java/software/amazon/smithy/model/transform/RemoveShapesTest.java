b348a185c9fc6167c69e68c9489239b15530172c
Chase Coalwell <chasecoa@amazon.com>
2020-04-17 11:15:06 -0700

    Remove unused import

--- smithy-model/src/test/java/software/amazon/smithy/model/transform/RemoveShapesTest.java
+++ smithy-model/src/test/java/software/amazon/smithy/model/transform/RemoveShapesTest.java
@@ -17,7 +17,6 @@ package software.amazon.smithy.model.transform;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collections;
