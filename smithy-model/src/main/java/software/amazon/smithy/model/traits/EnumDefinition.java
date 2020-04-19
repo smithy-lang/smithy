a4a9daf17edc4d5d7a33e6d99ed154290c71ea37
Michael Dowling <mtdowling@gmail.com>
2020-04-19 12:42:27 -0700

    Add support for selecting nested trait properties
    Nested trait properties can be selected using a "|" delimited syntax.
    Values or lists and objects can be "projected" using the special
    `(values)` property. Keys of objects can be projected using the
    special `(keys)` property.

--- smithy-model/src/main/java/software/amazon/smithy/model/traits/EnumDefinition.java
+++ smithy-model/src/main/java/software/amazon/smithy/model/traits/EnumDefinition.java
@@ -20,6 +20,10 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import software.amazon.smithy.model.node.Node;
+import software.amazon.smithy.model.node.ObjectNode;
+import software.amazon.smithy.model.node.StringNode;
+import software.amazon.smithy.model.node.ToNode;
 import software.amazon.smithy.utils.SmithyBuilder;
 import software.amazon.smithy.utils.Tagged;
 import software.amazon.smithy.utils.ToSmithyBuilder;
@@ -27,22 +31,26 @@ import software.amazon.smithy.utils.ToSmithyBuilder;
 /**
  * An enum definition for the enum trait.
  */
-public final class EnumDefinition implements ToSmithyBuilder<EnumDefinition>, Tagged {
+public final class EnumDefinition implements ToNode, ToSmithyBuilder<EnumDefinition>, Tagged {
+
     public static final String VALUE = "value";
     public static final String NAME = "name";
     public static final String DOCUMENTATION = "documentation";
     public static final String TAGS = "tags";
+    public static final String DEPRECATED = "deprecated";
 
     private final String value;
     private final String documentation;
     private final List<String> tags;
     private final String name;
+    private final boolean deprecated;
 
     private EnumDefinition(Builder builder) {
         value = SmithyBuilder.requiredState("value", builder.value);
         name = builder.name;
         documentation = builder.documentation;
         tags = new ArrayList<>(builder.tags);
+        deprecated = builder.deprecated;
     }
 
     public static Builder builder() {
@@ -61,6 +69,45 @@ public final class EnumDefinition implements ToSmithyBuilder<EnumDefinition>, Ta
         return Optional.ofNullable(documentation);
     }
 
+    public boolean isDeprecated() {
+        return deprecated;
+    }
+
+    @Override
+    public Node toNode() {
+        ObjectNode.Builder builder = Node.objectNodeBuilder();
+        builder.withMember(EnumDefinition.VALUE, getValue())
+                .withOptionalMember(EnumDefinition.NAME, getName().map(Node::from))
+                .withOptionalMember(EnumDefinition.DOCUMENTATION, getDocumentation().map(Node::from));
+
+        if (!tags.isEmpty()) {
+            builder.withMember(EnumDefinition.TAGS, Node.fromStrings(getTags()));
+        }
+
+        if (isDeprecated()) {
+            builder.withMember(EnumDefinition.DEPRECATED, true);
+        }
+
+        return builder.build();
+    }
+
+    public static EnumDefinition fromNode(Node node) {
+        ObjectNode value = node.expectObjectNode();
+        EnumDefinition.Builder builder = EnumDefinition.builder()
+                .value(value.expectStringMember(EnumDefinition.VALUE).getValue())
+                .name(value.getStringMember(EnumDefinition.NAME).map(StringNode::getValue).orElse(null))
+                .documentation(value.getStringMember(EnumDefinition.DOCUMENTATION)
+                                       .map(StringNode::getValue)
+                                       .orElse(null))
+                .deprecated(value.getBooleanMemberOrDefault(EnumDefinition.DEPRECATED));
+
+        value.getMember(EnumDefinition.TAGS).ifPresent(tags -> {
+            builder.tags(Node.loadArrayOfString(EnumDefinition.TAGS, tags));
+        });
+
+        return builder.build();
+    }
+
     @Override
     public List<String> getTags() {
         return tags;
@@ -68,7 +115,12 @@ public final class EnumDefinition implements ToSmithyBuilder<EnumDefinition>, Ta
 
     @Override
     public Builder toBuilder() {
-        return builder().value(value).tags(tags).documentation(documentation).name(name);
+        return builder()
+                .name(name)
+                .value(value)
+                .tags(tags)
+                .documentation(documentation)
+                .deprecated(deprecated);
     }
 
     @Override
@@ -81,12 +133,13 @@ public final class EnumDefinition implements ToSmithyBuilder<EnumDefinition>, Ta
         return value.equals(otherEnum.value)
                 && Objects.equals(name, otherEnum.name)
                 && Objects.equals(documentation, otherEnum.documentation)
-                && tags.equals(otherEnum.tags);
+                && tags.equals(otherEnum.tags)
+                && deprecated == otherEnum.deprecated;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(value, name, tags, documentation);
+        return Objects.hash(value, name, tags, documentation, deprecated);
     }
 
     /**
@@ -96,6 +149,7 @@ public final class EnumDefinition implements ToSmithyBuilder<EnumDefinition>, Ta
         private String value;
         private String documentation;
         private String name;
+        private boolean deprecated;
         private final List<String> tags = new ArrayList<>();
 
         @Override
@@ -131,6 +185,11 @@ public final class EnumDefinition implements ToSmithyBuilder<EnumDefinition>, Ta
 
         public Builder clearTags() {
             tags.clear();
+            return this;
+        }
+
+        public Builder deprecated(boolean deprecated) {
+            this.deprecated = deprecated;
             return this;
         }
     }
---
+++ smithy-model/src/test/resources/software/amazon/smithy/model/selector/nested-traits.smithy
@@ -0,0 +1,70 @@
+namespace smithy.example
+
+@enum([
+    {
+        value: "t2.nano",
+        name: "T2_NANO",
+        documentation: """
+            T2 instances are Burstable Performance
+            Instances that provide a baseline level of CPU
+            performance with the ability to burst above the
+            baseline.""",
+        tags: ["ebsOnly"]
+    },
+    {
+        value: "t2.micro",
+        name: "T2_MICRO",
+        documentation: """
+            T2 instances are Burstable Performance
+            Instances that provide a baseline level of CPU
+            performance with the ability to burst above the
+            baseline.""",
+        tags: ["ebsOnly"]
+    },
+    {
+        value: "m256.mega",
+        name: "M256_MEGA",
+        deprecated: true
+    }
+])
+@tags(["foo", "baz"])
+string EnumString
+
+@range(min: 1, max: 10)
+integer RangeInt1
+
+@range(min: 100, max: 1000)
+integer RangeInt2
+
+@externalDocumentation(
+    "Homepage": "https://www.example.com/",
+    "API Reference": "https://www.example.com/api-ref",
+)
+@enum([
+    {
+        value: "m256.mega",
+        name: "M256_MEGA",
+        tags: ["notEbs"]
+    }
+])
+@nestedTrait(foo: {foo: {bar: "hi"}})
+string DocumentedString1
+
+@documentation("Hi")
+@externalDocumentation("Foo": "https://www.anotherexample.com/")
+@nestedTrait(foo: {foo: {bar: "bye"}})
+@enum([{value: "m256.mega", tags: []}])
+string DocumentedString2
+
+@trait
+structure nestedTrait {
+    foo: MoreNesting,
+}
+
+structure MoreNesting {
+    foo: EvenMoreNesting,
+}
+
+structure EvenMoreNesting {
+    bar: String,
+}
