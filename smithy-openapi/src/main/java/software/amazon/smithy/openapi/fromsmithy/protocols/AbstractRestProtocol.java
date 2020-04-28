874747b26dc235157b1c39c9606c5752cdf0b4c8
Chase Coalwell <chasecoa@amazon.com>
2020-04-28 16:28:35 -0700

    Document API Gateway validation requirements

--- smithy-openapi/src/main/java/software/amazon/smithy/openapi/fromsmithy/protocols/AbstractRestProtocol.java
+++ smithy-openapi/src/main/java/software/amazon/smithy/openapi/fromsmithy/protocols/AbstractRestProtocol.java
@@ -406,6 +406,9 @@ abstract class AbstractRestProtocol<T extends Trait> implements OpenApiProtocol<
             ResponseObject.Builder responseBuilder,
             Shape operationOrError
     ) {
+        // API Gateway validation requires that in-line schemas must be objects
+        // or arrays. These schemas are synthesized as references so that
+        // any schemas with string types will pass validation.
         Schema schema = context.inlineOrReferenceSchema(binding.getMember());
         String shapeName = operationOrError.getId().getName();
         String synthesizedName = operationOrError instanceof OperationShape
