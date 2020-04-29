a047e38005c027be1af9bb0914b628a3404a68b2
Chase Coalwell <chasecoa@amazon.com>
2020-04-29 08:59:34 -0700

    Add httpPayload test targeting structures

---
+++ smithy-aws-apigateway-openapi/src/test/java/software/amazon/smithy/aws/apigateway/openapi/PayloadsTest.java
@@ -0,0 +1,28 @@
+package software.amazon.smithy.aws.apigateway.openapi;
+
+import org.junit.jupiter.api.Test;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.node.Node;
+import software.amazon.smithy.model.node.ObjectNode;
+import software.amazon.smithy.model.shapes.ShapeId;
+import software.amazon.smithy.openapi.OpenApiConfig;
+import software.amazon.smithy.openapi.fromsmithy.OpenApiConverter;
+import software.amazon.smithy.utils.IoUtils;
+
+public class PayloadsTest {
+    @Test
+    public void payloadsStructureTargetTest() {
+        Model model = Model.assembler(getClass().getClassLoader())
+                .discoverModels(getClass().getClassLoader())
+                .addImport(getClass().getResource("payloads.json"))
+                .assemble()
+                .unwrap();
+        OpenApiConfig config = new OpenApiConfig();
+        config.setService(ShapeId.from("example.smithy#MyService"));
+        ObjectNode result = OpenApiConverter.create().config(config).convertToNode(model);
+        Node expectedNode = Node.parse(IoUtils.toUtf8String(
+                getClass().getResourceAsStream("payloads.openapi.json")));
+
+        Node.assertEquals(result, expectedNode);
+    }
+}
