fc2efb87bead93e10a3ac078bdeead186ef75ae9
Chase Coalwell <chasecoa@amazon.com>
2020-09-17 10:37:17 -0700

    Add flattenNamespaces transformer
    Add license header

---
+++ smithy-build/src/test/java/software/amazon/smithy/build/transforms/FlattenNamespacesTest.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.smithy.build.transforms;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+import software.amazon.smithy.build.TransformContext;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.loader.Prelude;
+import software.amazon.smithy.model.node.Node;
+import software.amazon.smithy.model.node.ObjectNode;
+import software.amazon.smithy.model.shapes.Shape;
+import software.amazon.smithy.model.shapes.ShapeId;
+import software.amazon.smithy.utils.FunctionalUtils;
+
+public class FlattenNamespacesTest {
+
+    @Test
+    public void flattenNamespacesOfShapesConnectedToSpecifiedService() throws Exception {
+        Model model = Model.assembler()
+                .addImport(Paths.get(getClass().getResource("flatten-namespaces.json").toURI()))
+                .assemble()
+                .unwrap();
+        ObjectNode config = Node.objectNode()
+                .withMember("namespace", Node.from("ns.qux"))
+                .withMember("service", Node.from("ns.foo#MyService"));
+        TransformContext context = TransformContext.builder()
+                .model(model)
+                .settings(config)
+                .build();
+        Model result = new FlattenNamespaces().transform(context);
+        List<String> ids = result.shapes()
+                .filter(FunctionalUtils.not(Prelude::isPreludeShape))
+                .map(Shape::getId)
+                .map(Object::toString)
+                .collect(Collectors.toList());
+
+        assertThat(ids, containsInAnyOrder("ns.qux#MyService", "ns.qux#MyOperation", "ns.qux#MyOperationOutput",
+                "ns.qux#MyOperationOutput$foo", "ns.corge#UnconnectedFromService", "ns.grault#MyOperationOutput"));
+        assertThat(ids, not(containsInAnyOrder("ns.foo#MyService", "ns.bar#MyOperation", "ns.baz#MyOperationOutput",
+                "ns.baz#MyOperationOutput$foo", "ns.qux#UnconnectedFromService")));
+    }
+
+    @Test
+    public void includesAdditionalTaggedShapes() throws Exception {
+        Model model = Model.assembler()
+                .addImport(Paths.get(getClass().getResource("flatten-namespaces.json").toURI()))
+                .assemble()
+                .unwrap();
+        ObjectNode config = Node.objectNode()
+                .withMember("namespace", Node.from("ns.qux"))
+                .withMember("service", Node.from("ns.foo#MyService"))
+                .withMember("includeTagged", Node.arrayNode().withValue(Node.from("included")));
+        TransformContext context = TransformContext.builder()
+                .model(model)
+                .settings(config)
+                .build();
+        Model result = new FlattenNamespaces().transform(context);
+        List<String> ids = result.shapes()
+                .filter(FunctionalUtils.not(Prelude::isPreludeShape))
+                .map(Shape::getId)
+                .map(Object::toString)
+                .collect(Collectors.toList());
+
+        assertThat(ids, containsInAnyOrder("ns.qux#MyService", "ns.qux#MyOperation", "ns.qux#MyOperationOutput",
+                "ns.qux#MyOperationOutput$foo", "ns.qux#UnconnectedFromService", "ns.grault#MyOperationOutput"));
+        assertThat(ids, not(containsInAnyOrder("ns.foo#MyService", "ns.bar#MyOperation", "ns.baz#MyOperationOutput",
+                "ns.baz#MyOperationOutput$foo", "ns.corge#UnconnectedFromService")));
+    }
+
+    @Test
+    public void doesNotIncludeAdditionalTaggedShapesWhenTheyConflict() throws Exception {
+        Model model = Model.assembler()
+                .addImport(Paths.get(getClass().getResource("flatten-namespaces.json").toURI()))
+                .assemble()
+                .unwrap();
+        ObjectNode config = Node.objectNode()
+                .withMember("namespace", Node.from("ns.qux"))
+                .withMember("service", Node.from("ns.foo#MyService"))
+                .withMember("includeTagged", Node.arrayNode().withValue(Node.from("conflicting")));
+        TransformContext context = TransformContext.builder()
+                .model(model)
+                .settings(config)
+                .build();
+        Model result = new FlattenNamespaces().transform(context);
+        List<String> ids = result.shapes()
+                .filter(FunctionalUtils.not(Prelude::isPreludeShape))
+                .map(Shape::getId)
+                .map(Object::toString)
+                .collect(Collectors.toList());
+
+        assertThat(ids, containsInAnyOrder("ns.qux#MyService", "ns.qux#MyOperation", "ns.qux#MyOperationOutput",
+                "ns.qux#MyOperationOutput$foo", "ns.corge#UnconnectedFromService", "ns.grault#MyOperationOutput"));
+        assertThat(ids, not(containsInAnyOrder("ns.foo#MyService", "ns.bar#MyOperation", "ns.baz#MyOperationOutput",
+                "ns.baz#MyOperationOutput$foo", "ns.qux#UnconnectedFromService")));
+    }
+
+    @Test
+    public void doesNotModifyModelsWithoutServices() throws Exception {
+        Model model = Model.assembler()
+                .addImport(Paths.get(getClass().getResource("no-service.json").toURI()))
+                .assemble()
+                .unwrap();
+        TransformContext context = TransformContext.builder()
+                .model(model)
+                .settings(Node.objectNode().withMember("namespace", Node.from("ns.bar")))
+                .build();
+        Model result = new FlattenNamespaces().transform(context);
+
+        assertTrue(result.getShape(ShapeId.from("ns.foo#MyString")).isPresent());
+        assertFalse(result.getShape(ShapeId.from("ns.bar#MyString")).isPresent());
+    }
+}
