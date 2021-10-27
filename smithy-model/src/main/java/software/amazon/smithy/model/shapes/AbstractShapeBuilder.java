6195f1f4080488ef875dcfb42435b149ad7aa064
JordonPhillips <JordonPhillips@users.noreply.github.com>
2021-10-27 16:40:46 +0200

    Support inline operation IO shape definitions
    This adds support for inline definitions of operation input and
    output shapes. This somewhat reduces necessary boilerplate, and
    makes the model a bit easier to read since the high level info
    for an operation is now all in one place.
    
    This also updates the IDL serializer to use inlined shapes
    wherever possible.

--- smithy-model/src/main/java/software/amazon/smithy/model/shapes/AbstractShapeBuilder.java
+++ smithy-model/src/main/java/software/amazon/smithy/model/shapes/AbstractShapeBuilder.java
@@ -18,7 +18,6 @@ package software.amazon.smithy.model.shapes;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
