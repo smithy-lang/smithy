d3efb08d067f0d7bb728808c35f2afc6b0fdbfd2
JordonPhillips <JordonPhillips@users.noreply.github.com>
2022-01-07 17:50:20 +0100

    Require with to be on the same line

--- smithy-model/src/main/java/software/amazon/smithy/model/shapes/Shape.java
+++ smithy-model/src/main/java/software/amazon/smithy/model/shapes/Shape.java
@@ -96,9 +96,11 @@ public abstract class Shape implements FromSourceLocation, Tagged, ToShapeId, Co
         }
         if (!invalid.isEmpty()) {
             String invalidList = String.join("`, `", invalid);
-            throw new IllegalStateException(String.format(
+            throw new SourceException(String.format(
                     "Mixins may only be mixed into shapes of the same type. The following mixins were applied to the "
-                            + "%s shape `%s` which are not %1$s shapes: [`%s`]", getType(), getId(), invalidList));
+                            + "%s shape `%s` which are not %1$s shapes: [`%s`]", getType(), getId(), invalidList),
+                    source
+            );
         }
     }
 
@@ -106,6 +108,7 @@ public abstract class Shape implements FromSourceLocation, Tagged, ToShapeId, Co
             AbstractShapeBuilder<?, ?> builder,
             String name
     ) {
+        // Get the most recently introduced mixin member with the given name.
         MemberShape mixedMember = null;
         for (Shape shape : builder.getMixins().values()) {
             for (MemberShape member : shape.members()) {
@@ -121,7 +124,10 @@ public abstract class Shape implements FromSourceLocation, Tagged, ToShapeId, Co
             }
         }
         if (mixedMember == null) {
-            throw new IllegalStateException(String.format("Missing required member of shape `%s`: %s", getId(), name));
+            throw new SourceException(
+                    String.format("Missing required member of shape `%s`: %s", getId(), name),
+                    builder.getSourceLocation()
+            );
         }
         return mixedMember;
     }
