c8cc25e22a01f1349d026713c47fe081ba29eedf
JordonPhillips <JordonPhillips@users.noreply.github.com>
2022-01-07 18:12:13 +0100

    Deprecate StutteredShapeName in favor of RepeateShapeName

--- smithy-linters/src/main/java/software/amazon/smithy/linters/RepeatedShapeNameValidator.java
+++ smithy-linters/src/main/java/software/amazon/smithy/linters/RepeatedShapeNameValidator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -28,12 +28,15 @@ import software.amazon.smithy.model.validation.AbstractValidator;
 import software.amazon.smithy.model.validation.ValidationEvent;
 import software.amazon.smithy.model.validation.ValidatorService;
 
-@Deprecated
-public final class StutteredShapeNameValidator extends AbstractValidator {
+/**
+ * Validates that structure members and union member names do not
+ * repeat their shape name as prefixes of their member or tag names.
+ */
+public final class RepeatedShapeNameValidator extends AbstractValidator {
 
     public static final class Provider extends ValidatorService.Provider {
         public Provider() {
-            super(StutteredShapeNameValidator.class, StutteredShapeNameValidator::new);
+            super(RepeatedShapeNameValidator.class, RepeatedShapeNameValidator::new);
         }
     }
 
@@ -52,15 +55,14 @@ public final class StutteredShapeNameValidator extends AbstractValidator {
         String lowerCaseShapeName = shapeName.toLowerCase(Locale.US);
         return memberNames.stream()
                 .filter(memberName -> memberName.toLowerCase(Locale.US).startsWith(lowerCaseShapeName))
-                .map(memberName -> stutteredMemberName(model, shape, shapeName, memberName))
+                .map(memberName -> repeatedMemberName(model, shape, shapeName, memberName))
                 .collect(Collectors.toList());
     }
 
-    private ValidationEvent stutteredMemberName(Model model, Shape shape, String shapeName, String memberName) {
-        Shape member = model.getShape(shape.getId().withMember(memberName)).orElseThrow(
-                () -> new RuntimeException("Invalid member name for shape: " + shape + ", " + memberName));
+    private ValidationEvent repeatedMemberName(Model model, Shape shape, String shapeName, String memberName) {
+        Shape member = model.expectShape(shape.getId().withMember(memberName));
         return warning(member, String.format(
-                "The `%s` %s shape stutters its name in the member `%s`; %2$s member names should not be "
-                + "prefixed with the %2$s name.", shapeName, shape.getType(), memberName));
+                "The `%s` %s shape repeats its name in the member `%s`; %2$s member names should not be "
+                        + "prefixed with the %2$s name.", shapeName, shape.getType(), memberName));
     }
 }
