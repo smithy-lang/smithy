cc02be5e4da6ef4ca7ea3cac079d37d98794d357
Michael Dowling <mtdowling@gmail.com>
2022-02-16 16:54:43 -0800

    Add built-in support for Symbol formatting
    CodegenWriter is supposed to be an opinionated implementation of
    CodeWriter that automatically brings Symbols and imports together with a
    CodeWriter. This commit adds a built-in "T" formatter for handling
    Symbol and SymbolReference objects. This should work for most Smithy
    codegen implementations, but the ones that it doesn't can either
    override the "T" formatter or not use CodegenWriter.

--- smithy-codegen-core/src/test/java/software/amazon/smithy/codegen/core/writer/CodegenWriterTest.java
+++ smithy-codegen-core/src/test/java/software/amazon/smithy/codegen/core/writer/CodegenWriterTest.java
@@ -18,6 +18,7 @@ package software.amazon.smithy.codegen.core.writer;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.not;
 
@@ -112,5 +113,43 @@ public class CodegenWriterTest {
         assertThat(writer.getImportContainer().imports, hasKey("List"));
         assertThat(writer.getImportContainer().imports, hasKey("MyString"));
         assertThat(writer.getImportContainer().imports.get("MyString"), equalTo("java.lang.String"));
+    }
+
+    @Test
+    public void formatsSymbolsWithNoNamespaceRelativization() {
+        MyWriter writer = new MyWriter("java.lang");
+        // This symbol should *not* be relativized.
+        Symbol string = Symbol.builder().name("String").namespace("java.lang", ".").build();
+        writer.write("$T", string);
+
+        assertThat(writer.toString(), equalTo("java.lang.String\n"));
+    }
+
+    @Test
+    public void formatsSymbolsWithNamespaceRelativization() {
+        MyWriter writer = new MyWriter("java.lang");
+        // normally the constructor would call this automatically, but this is a test case!
+        writer.setRelativizeSymbols("java.lang");
+        // This symbol should be relativized.
+        Symbol string = Symbol.builder().name("String").namespace("java.lang", ".").build();
+        writer.write("$T", string);
+
+        assertThat(writer.toString(), equalTo("String\n"));
+    }
+
+    @Test
+    public void formatsSymbolReferences() {
+        MyWriter writer = new MyWriter("com.foo");
+        Symbol string = Symbol.builder().name("String").namespace("example.foo", ".").build();
+        SymbolReference reference = SymbolReference.builder()
+                .alias("Str")
+                .symbol(string)
+                .build();
+        writer.write("$T", reference);
+
+        assertThat(writer.toString(), equalTo("Str\n"));
+
+        // The reference automatically adds imports.
+        assertThat(writer.getImportContainer().imports, hasEntry("Str", string.toString()));
     }
 }
