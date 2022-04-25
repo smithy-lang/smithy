061fc438dae82bdbcf0a26503dcfe61c219a8e7c
Michael Dowling <mtdowling@gmail.com>
2022-04-24 23:53:20 -0700

    Optimize slightly shape ID conflict validation

--- smithy-model/src/main/java/software/amazon/smithy/model/validation/validators/ShapeIdConflictValidator.java
+++ smithy-model/src/main/java/software/amazon/smithy/model/validation/validators/ShapeIdConflictValidator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -15,13 +15,14 @@
 
 package software.amazon.smithy.model.validation.validators;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import software.amazon.smithy.model.Model;
-import software.amazon.smithy.model.shapes.Shape;
 import software.amazon.smithy.model.shapes.ShapeId;
 import software.amazon.smithy.model.validation.AbstractValidator;
 import software.amazon.smithy.model.validation.ValidationEvent;
@@ -33,30 +34,31 @@ import software.amazon.smithy.model.validation.ValidationEvent;
 public final class ShapeIdConflictValidator extends AbstractValidator {
     @Override
     public List<ValidationEvent> validate(Model model) {
-        Map<String, List<Shape>> conflicts = model.shapes()
-                .collect(Collectors.groupingBy(shape -> shape.getId().toString().toLowerCase(Locale.US)))
-                .entrySet().stream()
-                .filter(entry -> entry.getValue().size() > 1)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        // Set initial capacity of the HashMap under the assumption most models will have valid and unique IDs.
+        int initialCapacity = (int) Math.ceil(model.getShapeIds().size() * 0.75 + 1);
+        Map<String, Collection<ShapeId>> conflicts = new HashMap<>(initialCapacity);
 
-        return conflicts.values().stream()
-                .flatMap(this::emitEvents)
-                .collect(Collectors.toList());
-    }
+        for (ShapeId id : model.getShapeIds()) {
+            // Most shapes will have no conflicts, so set the initial capacity of the ArrayList to 1.
+            conflicts.computeIfAbsent(id.toString().toLowerCase(Locale.ENGLISH), i -> new ArrayList<>(1)).add(id);
+        }
 
-    private Stream<ValidationEvent> emitEvents(List<Shape> collidingShapes) {
-        List<ShapeId> collidingIds = collidingShapes.stream()
-                .map(Shape::getId)
-                .sorted()
-                .collect(Collectors.toList());
-        return collidingShapes.stream().map(value -> {
-            String collideString = collidingIds.stream()
-                    .filter(id -> !id.equals(value.getId()))
-                    .map(id -> "`" + id + "`")
-                    .collect(Collectors.joining(", "));
-            return error(value, String.format(
-                    "Shape ID `%s` conflicts with other shape IDs in the model: [%s]",
-                    value.getId(), collideString));
-        });
+        List<ValidationEvent> events = new ArrayList<>();
+        for (Map.Entry<String, Collection<ShapeId>> entry : conflicts.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                for (ShapeId value : entry.getValue()) {
+                    String collideString = entry.getValue().stream()
+                            .filter(id -> !id.equals(value))
+                            .sorted()
+                            .map(id -> "`" + id + "`")
+                            .collect(Collectors.joining(", "));
+                    events.add(error(model.expectShape(value), String.format(
+                            "Shape ID `%s` conflicts with other shape IDs in the model: [%s]",
+                            value, collideString)));
+                }
+            }
+        }
+
+        return events;
     }
 }
