faec5f76231aedd4d058cb3b7422176ff4d1c8bd
Michael Dowling <mtdowling@gmail.com>
2022-06-07 20:13:10 -0700

    Fix tests on Windows

--- smithy-model/src/main/java/software/amazon/smithy/model/validation/testrunner/SmithyTestCase.java
+++ smithy-model/src/main/java/software/amazon/smithy/model/validation/testrunner/SmithyTestCase.java
@@ -147,7 +147,7 @@ public final class SmithyTestCase {
 
     private static List<ValidationEvent> loadExpectedEvents(String errorsFileLocation) {
         String contents = IoUtils.readUtf8File(errorsFileLocation);
-        return Arrays.stream(contents.split(System.lineSeparator()))
+        return Arrays.stream(contents.split("\r?\n"))
                 .filter(line -> !line.trim().isEmpty())
                 .map(SmithyTestCase::parseValidationEvent)
                 .collect(Collectors.toList());
