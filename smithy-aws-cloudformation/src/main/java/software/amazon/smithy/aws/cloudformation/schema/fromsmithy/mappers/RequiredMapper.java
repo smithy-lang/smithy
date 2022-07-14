61d7851400e0deadbcb5ec920738cf85ec6aaa76
Manuel Sugawara <108146565+sugmanue@users.noreply.github.com>
2022-07-14 09:53:34 -0700

    Do not map required members that are only used for output (#1295)
    For this we use the mutabilities set computed upstream and include
    only those that have WRITE or CREATE mutability.

--- smithy-aws-cloudformation/src/main/java/software/amazon/smithy/aws/cloudformation/schema/fromsmithy/mappers/RequiredMapper.java
+++ smithy-aws-cloudformation/src/main/java/software/amazon/smithy/aws/cloudformation/schema/fromsmithy/mappers/RequiredMapper.java
@@ -15,9 +15,13 @@
 
 package software.amazon.smithy.aws.cloudformation.schema.fromsmithy.mappers;
 
+import java.util.Optional;
 import software.amazon.smithy.aws.cloudformation.schema.fromsmithy.CfnMapper;
 import software.amazon.smithy.aws.cloudformation.schema.fromsmithy.Context;
 import software.amazon.smithy.aws.cloudformation.schema.model.ResourceSchema;
+import software.amazon.smithy.aws.cloudformation.traits.CfnResource;
+import software.amazon.smithy.aws.cloudformation.traits.CfnResourceIndex;
+import software.amazon.smithy.aws.cloudformation.traits.CfnResourceProperty;
 import software.amazon.smithy.model.Model;
 import software.amazon.smithy.model.shapes.MemberShape;
 import software.amazon.smithy.model.shapes.StructureShape;
@@ -38,15 +42,27 @@ public final class RequiredMapper implements CfnMapper {
         if (context.getConfig().getDisableRequiredPropertyGeneration()) {
             return;
         }
-
-        // If any of the pseudo-resource structure's members are required,
-        // then require the CFN property as well.
+        // If any of the pseudo-resource structure's members are
+        // required and are not only used in output structures then
+        // require the CFN property as well.
         Model model = context.getModel();
         StructureShape resourceStructure = context.getResourceStructure();
         for (MemberShape member : resourceStructure.members()) {
             if (member.getMemberTrait(model, RequiredTrait.class).isPresent()) {
-                resourceSchema.addRequired(context.getJsonSchemaConverter().toPropertyName(member));
+                if (hasWriteOrCreateMutability(context, member)) {
+                    resourceSchema.addRequired(context.getJsonSchemaConverter().toPropertyName(member));
+                }
             }
         }
+    }
+
+    private boolean hasWriteOrCreateMutability(Context context, MemberShape member) {
+        CfnResource cfnResource = context.getCfnResource();
+        String memberId = member.getId().toShapeId().getMember().get();
+        Optional<CfnResourceProperty> cfnResourceProperty = cfnResource.getProperty(memberId);
+        return cfnResourceProperty.map(CfnResourceProperty::getMutabilities)
+                .map(mutabilities -> mutabilities.contains(CfnResourceIndex.Mutability.WRITE)
+                        || mutabilities.contains(CfnResourceIndex.Mutability.CREATE))
+                .orElse(false);
     }
 }
