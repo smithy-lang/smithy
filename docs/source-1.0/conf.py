e5d98d71df272b754c4d2ab1a69191ce73224c35
Michael Dowling <mtdowling@gmail.com>
2022-07-14 13:47:17 -0700

    Add banner about deprecated docs

--- docs/source-1.0/conf.py
+++ docs/source-1.0/conf.py
@@ -4,3 +4,6 @@ exec(compile(source=open(shared_config).read(), filename=shared_config, mode='ex
 # Place any version specific overrides here.
 release = u'1.0'
 version = release
+
+html_theme_options['announcement'] = '''<strong>⚠️ You are viewing version 1.0.</strong><br/>
+Version 2.0 is available at <a href="https://awslabs.github.io/smithy/2.0/">https://awslabs.github.io/smithy/2.0/</a>.'''
