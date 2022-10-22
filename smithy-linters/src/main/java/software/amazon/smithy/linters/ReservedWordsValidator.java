b25767a422032f75efa3e29e38fd47beae05b72f
Michael Dowling <mtdowling@gmail.com>
2022-10-21 17:11:42 -0700

    Add ability to lint based on word boundaries
    This commit introduces a new syntax for matching words with the
    ReservedWords linter and is intended to be used with the upcoming
    sensitive words linter defined in #1364.
    
    In addition to supporting wildcard searches ("*" prefix, suffix,
    and contains), we now support matching based on word boundaries.
    
    This commit introduces the "terms" keyword for word boundary
    searches and adds dedicated abstractions for word boundary and
    wildcard matching.
    
    For example, "access key id" will match "AccessKeyId",
    "access_key_id", "accessKeyID", "access_key_id100", "AccesKeyIDValue".
    It will also match when all the words are concatenated together:
    "accesskeyid". However, it will not match "accesskey_id" because it
    only has two word boundaries ("accesskey" and "id").

--- smithy-linters/src/main/java/software/amazon/smithy/linters/ReservedWordsValidator.java
+++ smithy-linters/src/main/java/software/amazon/smithy/linters/ReservedWordsValidator.java
@@ -20,11 +20,7 @@ import static java.lang.String.format;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 import java.util.Optional;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import software.amazon.smithy.model.Model;
 import software.amazon.smithy.model.node.NodeMapper;
 import software.amazon.smithy.model.selector.Selector;
@@ -34,7 +30,6 @@ import software.amazon.smithy.model.validation.AbstractValidator;
 import software.amazon.smithy.model.validation.Severity;
 import software.amazon.smithy.model.validation.ValidationEvent;
 import software.amazon.smithy.model.validation.ValidatorService;
-import software.amazon.smithy.utils.OptionalUtils;
 
 /**
  * Emits validation events for a configuration of reserved words.
@@ -48,6 +43,7 @@ import software.amazon.smithy.utils.OptionalUtils;
  *             <li>words: ([string]) A list of words that are
  *             case-insensitively reserved. Leading and trailing wildcards
  *             ("*") are supported.
+ *             <li>terms: ([string]) A list of word boundary terms to test.</li>
  *             <li>selector: (string) Specifies a selector for this
  *             configuration. Defaults to validating all shapes, including
  *             member names.
@@ -83,9 +79,10 @@ public final class ReservedWordsValidator extends AbstractValidator {
      * A single reserved words configuration.
      */
     public static final class ReservedWords {
-        private List<String> words = Collections.emptyList();
         private Selector selector = Selector.IDENTITY;
         private String reason = "";
+        private final WildcardMatcher wildcardMatcher = new WildcardMatcher();
+        private final WordBoundaryMatcher wordMatcher = new WordBoundaryMatcher();
 
         /**
          * Sets the list of reserved word definitions.
@@ -96,16 +93,16 @@ public final class ReservedWordsValidator extends AbstractValidator {
          * @param words Words to set.
          */
         public void setWords(List<String> words) {
-            this.words = new ArrayList<>(words.size());
-            for (String word : words) {
-                if (word.equals("*")) {
-                    throw new IllegalArgumentException("Reservations cannot be made against '*'");
-                }
-                if (CONTAINS_INNER_WILDCARD.matcher(word).find()) {
-                    throw new IllegalArgumentException("Only preceding and trailing wildcards ('*') are supported.");
-                }
-                this.words.add(word.toLowerCase(Locale.ENGLISH));
-            }
+            words.forEach(wildcardMatcher::addSearch);
+        }
+
+        /**
+         * Sets the list of reserved word terms to match based on word boundaries.
+         *
+         * @param terms Terms to set.
+         */
+        public void setTerms(List<String> terms) {
+            terms.forEach(wordMatcher::addSearch);
         }
 
         /**
@@ -126,8 +123,10 @@ public final class ReservedWordsValidator extends AbstractValidator {
             this.reason = reason;
         }
 
-        private Stream<ValidationEvent> validate(Model model) {
-            return selector.select(model).stream().flatMap(shape -> OptionalUtils.stream(validateShape(shape)));
+        private void validate(Model model, List<ValidationEvent> events) {
+            for (Shape shape : selector.select(model)) {
+                validateShape(shape).ifPresent(events::add);
+            }
         }
 
         private Optional<ValidationEvent> validateShape(Shape shape) {
@@ -139,28 +138,13 @@ public final class ReservedWordsValidator extends AbstractValidator {
         }
 
         /**
-         * Checks a passed word against the list of reserved words in this
-         * configuration. Validates these in a case-insensitive manner, and
-         * supports starting and ending wildcards '*'.
+         * Checks a passed word against the reserved words in this configuration.
          *
          * @param word A value that may be reserved.
          * @return Returns true if the word is reserved by this configuration
          */
         private boolean isReservedWord(String word) {
-            String compare = word.toLowerCase(Locale.US);
-            return words.stream().anyMatch(reservation -> {
-                // Comparisons against '*' have been rejected at configuration load.
-                if (reservation.startsWith("*")) {
-                    if (reservation.endsWith("*")) {
-                        return compare.contains(reservation.substring(1, reservation.lastIndexOf("*")));
-                    }
-                    return compare.endsWith(reservation.substring(1));
-                }
-                if (reservation.endsWith("*")) {
-                    return compare.startsWith(reservation.substring(0, reservation.lastIndexOf("*")));
-                }
-                return compare.equals(reservation);
-            });
+            return wildcardMatcher.test(word) || wordMatcher.test(word);
         }
 
         private ValidationEvent emit(Shape shape, String word, String reason) {
@@ -182,21 +166,18 @@ public final class ReservedWordsValidator extends AbstractValidator {
         }
     }
 
-    private static final Pattern CONTAINS_INNER_WILDCARD = Pattern.compile("^.+\\*.+$");
-
     private final Config config;
 
     private ReservedWordsValidator(Config config) {
         this.config = config;
-
-        if (config.getReserved().isEmpty()) {
-            throw new IllegalArgumentException("Missing `reserved` words");
-        }
     }
 
     @Override
     public List<ValidationEvent> validate(Model model) {
-        return config.getReserved().stream().flatMap(reservation -> reservation.validate(model))
-                .collect(Collectors.toList());
+        List<ValidationEvent> events = new ArrayList<>();
+        for (ReservedWords reserved : config.getReserved()) {
+            reserved.validate(model, events);
+        }
+        return events;
     }
 }
---
+++ smithy-linters/src/test/java/software/amazon/smithy/linters/WordBoundaryMatcherTest.java
@@ -0,0 +1,97 @@
+package software.amazon.smithy.linters;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class WordBoundaryMatcherTest {
+
+    @ParameterizedTest
+    @MethodSource("patternProvider")
+    public void matchesPatterns(String text, String pattern, boolean match) {
+        WordBoundaryMatcher matcher = new WordBoundaryMatcher();
+        matcher.addSearch(pattern);
+
+        assertThat("'" + text + "' matches '" + pattern + '\'', matcher.test(text), is(match));
+    }
+
+    public static Stream<Arguments> patternProvider() {
+        return Stream.of(
+            // Can't match empty or null.
+            Arguments.of("", "access key id", false),
+            Arguments.of(null, "access key id", false),
+
+            // Good word matches.
+            Arguments.of("accessKeyId", "access key id", true),
+            Arguments.of("accesskeyid", "access key id", true),
+            Arguments.of("access_key_id", "access key id", true),
+            Arguments.of("access_key_ID", "access key id", true),
+            Arguments.of("accessKey__Id", "access key id", true),
+
+            // Tricky word boundary matches.
+            Arguments.of("accessKey__Id", "access key id", true),
+            Arguments.of("accessKey__Id", "access key id", true),
+            Arguments.of("accessKey__Id", "access key id", true),
+            Arguments.of("accessKey__Id", "access key id", true),
+            Arguments.of("accessKey__Id", "access key id", true),
+            Arguments.of("accessKey__Id", "access key id", true),
+            Arguments.of("accessKey__Id", "access key id", true),
+            Arguments.of("access:Key:Id", "access key id", true),
+            Arguments.of("access Key Id", "access key id", true),
+            Arguments.of("access-Key-Id", "access key id", true),
+            Arguments.of("access.Key.Id200", "access key id", true),
+            Arguments.of("AccessKeyIDValue", "access key id", true),
+            Arguments.of("__AccessKeyIDValue__", "access key id", true),
+            Arguments.of("zip", "zip", true),
+            Arguments.of("unzip", "zip", false),
+            Arguments.of("zipCode", "zip", true),
+
+            // No match because zipcode is parsed as one word.
+            Arguments.of("zipcode", "zip", false),
+
+            // No match is found because "accesskey_id" is split into "accesskey id"
+            Arguments.of("foo accesskey_id", "access key id", false),
+
+            // Cases where no match is found and the word counts differ.
+            Arguments.of("string", "this is too long to match", false),
+            Arguments.of("this is not a match", "no", false),
+
+            // An exact match.
+            Arguments.of("string", "string", true),
+
+            Arguments.of("foo_bar_baz", "bar", true),
+            Arguments.of("foo_baz_bar", "bar", true),
+            Arguments.of("foo_bazbar", "bar", false),
+            Arguments.of("bazbarbaz", "bar", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidPatternProvider")
+    public void validatesSyntax(String invalidPattern) {
+        WordBoundaryMatcher matcher = new WordBoundaryMatcher();
+
+        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class,
+                                () -> matcher.addSearch(invalidPattern),
+                                invalidPattern);
+
+        // All syntax errors should show the invalid pattern.
+        assertThat(e.getMessage(), containsString(invalidPattern));
+    }
+
+    public static Stream<Arguments> invalidPatternProvider() {
+        return Stream.of(
+            Arguments.of("foo  bar"),
+            Arguments.of(" foo bar "),
+            Arguments.of("   foo"),
+            Arguments.of("foo_bar"),   // non alphanumeric
+            Arguments.of("foo+bar")    // non alphanumeric
+        );
+    }
+}
