f08cc553bd40a3ac715b838fd528c00b11cb5296
Bigo <1781140+crisidev@users.noreply.github.com>
2023-01-18 23:02:17 +0000

    Add support for wire formats

--- smithy-protocols-traits/src/main/java/software/amazon/smithy/protocols/traits/SmithyProtocolTrait.java
+++ smithy-protocols-traits/src/main/java/software/amazon/smithy/protocols/traits/SmithyProtocolTrait.java
@@ -28,22 +28,25 @@ import software.amazon.smithy.utils.ListUtils;
 /**
  * Represents a configurable Smithy protocol trait.
  *
- * <p>Subclasses are traits that allow the supported HTTP versions and
- * eventStream HTTP versions to be customized.
+ * <p>Subclasses are traits that allow the supported HTTP versions,
+ * eventStream HTTP versions and wire format to be customized.
  */
 public abstract class SmithyProtocolTrait extends AbstractTrait {
 
     private static final String HTTP = "http";
     private static final String EVENT_STREAM_HTTP = "eventStreamHttp";
+    private static final String FORMAT = "format";
 
     private final List<String> http;
     private final List<String> eventStreamHttp;
+    private final List<String> format;
 
     // package-private constructor (at least for now)
     SmithyProtocolTrait(ShapeId id, Builder<?, ?> builder) {
         super(id, builder.getSourceLocation());
         http = ListUtils.copyOf(builder.http);
         eventStreamHttp = ListUtils.copyOf(builder.eventStreamHttp);
+        format = ListUtils.copyOf(builder.format);
     }
 
     /**
@@ -65,6 +68,15 @@ public abstract class SmithyProtocolTrait extends AbstractTrait {
         return eventStreamHttp;
     }
 
+    /**
+     * Gets the priority ordered list of supported wire formats.
+     *
+     * @return Returns the supported wire formats.
+     */
+    public List<String> getFormat() {
+        return format;
+    }
+
     @Override
     protected Node createNode() {
         ObjectNode.Builder builder = Node.objectNodeBuilder();
@@ -75,6 +87,10 @@ public abstract class SmithyProtocolTrait extends AbstractTrait {
 
         if (!getEventStreamHttp().isEmpty()) {
             builder.withMember(EVENT_STREAM_HTTP, Node.fromStrings(getEventStreamHttp()));
+        }
+
+        if (!getFormat().isEmpty()) {
+            builder.withMember(FORMAT, Node.fromStrings(getFormat()));
         }
 
         return builder.build();
@@ -90,6 +106,7 @@ public abstract class SmithyProtocolTrait extends AbstractTrait {
 
         private final List<String> http = new ArrayList<>();
         private final List<String> eventStreamHttp = new ArrayList<>();
+        private final List<String> format = new ArrayList<>();
 
         /**
          * Sets the list of supported HTTP protocols.
@@ -118,6 +135,19 @@ public abstract class SmithyProtocolTrait extends AbstractTrait {
         }
 
         /**
+         * Sets the list of supported wire formats.
+         *
+         * @param format Wire format to set and replace.
+         * @return Returns the builder.
+         */
+        @SuppressWarnings("unchecked")
+        public B format(List<String> format) {
+            this.format.clear();
+            this.format.addAll(format);
+            return (B) this;
+        }
+
+        /**
          * Updates the builder from a Node.
          *
          * @param node Node object that must be a valid {@code ObjectNode}.
@@ -132,6 +162,10 @@ public abstract class SmithyProtocolTrait extends AbstractTrait {
             objectNode.getArrayMember(EVENT_STREAM_HTTP)
                     .map(values -> Node.loadArrayOfString(EVENT_STREAM_HTTP, values))
                     .ifPresent(this::eventStreamHttp);
+            objectNode.getArrayMember(FORMAT)
+                    .map(values -> Node.loadArrayOfString(FORMAT, values))
+                    .ifPresent(this::format);
+                
             return (B) this;
         }
     }
