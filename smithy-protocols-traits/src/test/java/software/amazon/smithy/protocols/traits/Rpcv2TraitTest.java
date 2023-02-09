3eeb387f587f85c554d0fc777bcee7e057567dc3
Bigo <1781140+crisidev@users.noreply.github.com>
2023-02-09 18:31:15 +0000

    Format also tests

--- smithy-protocols-traits/src/test/java/software/amazon/smithy/protocols/traits/Rpcv2TraitTest.java
+++ smithy-protocols-traits/src/test/java/software/amazon/smithy/protocols/traits/Rpcv2TraitTest.java
@@ -1,16 +1,14 @@
 /*
  * Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except
+ * in compliance with the License. A copy of the License is located at
  *
- *  http://aws.amazon.com/apache2.0
+ * http://aws.amazon.com/apache2.0
  *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package software.amazon.smithy.protocols.traits;
@@ -30,7 +28,8 @@ public class Rpcv2TraitTest {
     public void loadsTraitWithDefaults() {
         Node node = Node.objectNode();
         TraitFactory provider = TraitFactory.createServiceFactory();
-        Optional<Trait> trait = provider.createTrait(Rpcv2Trait.ID, ShapeId.from("ns.foo#foo"), node);
+        Optional<Trait> trait =
+                provider.createTrait(Rpcv2Trait.ID, ShapeId.from("ns.foo#foo"), node);
 
         Assertions.assertTrue(trait.isPresent());
         Assertions.assertTrue(trait.get() instanceof Rpcv2Trait);
