01a2d5b1bb69ccd9e1c8b0d3fa70cd7634fb1b4a
Hunter Mellema <124718352+hpmellema@users.noreply.github.com>
2023-02-22 15:19:53 -0700

    Fix warnings in AST Loader for Resource and Operation Shapes with mixins (#1626)
    * fix smithy validate throwing warning on operation mixins
    
    * fix validation for resource shapes with mixin to no longer throw warning
    
    * also fix service, resource, collection, and simple shapes
    
    * Update smithy-model error files to reflect updated warning message

--- smithy-model/src/test/java/software/amazon/smithy/model/loader/AstModelLoaderTest.java
+++ smithy-model/src/test/java/software/amazon/smithy/model/loader/AstModelLoaderTest.java
@@ -27,20 +27,83 @@ public class AstModelLoaderTest {
     @Test
     public void failsToLoadPropertiesFromV1() {
         ValidatedResult<Model> model = Model.assembler()
-                .addImport(getClass().getResource("invalid/properties-v2-only.json"))
-                .assemble();
+            .addImport(getClass().getResource("invalid/properties-v2-only.json"))
+            .assemble();
         assertEquals(1, model.getValidationEvents(Severity.ERROR).size());
         assertTrue(model.getValidationEvents(Severity.ERROR).get(0).getMessage()
-                .contains("Resource properties can only be used with Smithy version 2 or later."));
+            .contains("Resource properties can only be used with Smithy version 2 or later."));
     }
 
     @Test
     public void doesNotFailOnEmptyApply() {
         // Empty apply statements are pointless but shouldn't break the loader.
         Model.assembler()
-                .addImport(getClass().getResource("ast-empty-apply-1.json"))
-                .addImport(getClass().getResource("ast-empty-apply-2.json"))
-                .assemble()
-                .unwrap();
+            .addImport(getClass().getResource("ast-empty-apply-1.json"))
+            .addImport(getClass().getResource("ast-empty-apply-2.json"))
+            .assemble()
+            .unwrap();
+    }
+
+    @Test
+    public void allowsMixinsOnOperationsWithoutWarningOrError() {
+        ValidatedResult<Model> model = Model.assembler()
+            .addImport(getClass().getResource("mixins/operation-mixins.json"))
+            .assemble();
+        assertEquals(0, model.getValidationEvents(Severity.WARNING).size());
+        assertEquals(0, model.getValidationEvents(Severity.ERROR).size());
+    }
+
+    @Test
+    public void allowsMixinsOnResourcesWithoutWarningOrError() {
+        ValidatedResult<Model> model = Model.assembler()
+            .addImport(getClass().getResource("mixins/resource-mixins.json"))
+            .assemble();
+        assertEquals(0, model.getValidationEvents(Severity.WARNING).size());
+        assertEquals(0, model.getValidationEvents(Severity.ERROR).size());
+    }
+
+    @Test
+    public void allowsMixinsOnServiceWithoutWarningOrError() {
+        ValidatedResult<Model> model = Model.assembler()
+            .addImport(getClass().getResource("mixins/service-mixins.json"))
+            .assemble();
+        assertEquals(0, model.getValidationEvents(Severity.WARNING).size());
+        assertEquals(0, model.getValidationEvents(Severity.ERROR).size());
+    }
+
+    @Test
+    public void allowsMixinsOnStructuresWithoutWarningOrError() {
+        ValidatedResult<Model> model = Model.assembler()
+            .addImport(getClass().getResource("mixins/structure-mixins.json"))
+            .assemble();
+        assertEquals(0, model.getValidationEvents(Severity.WARNING).size());
+        assertEquals(0, model.getValidationEvents(Severity.ERROR).size());
+    }
+
+    @Test
+    public void allowsMixinsOnUnionsWithoutWarningOrError() {
+        ValidatedResult<Model> model = Model.assembler()
+            .addImport(getClass().getResource("mixins/union-mixins.json"))
+            .assemble();
+        assertEquals(0, model.getValidationEvents(Severity.WARNING).size());
+        assertEquals(0, model.getValidationEvents(Severity.ERROR).size());
+    }
+
+    @Test
+    public void allowsMixinsOnSimpleShapesWithoutWarningOrError() {
+        ValidatedResult<Model> model = Model.assembler()
+            .addImport(getClass().getResource("mixins/simple-shape-mixins.json"))
+            .assemble();
+        assertEquals(0, model.getValidationEvents(Severity.WARNING).size());
+        assertEquals(0, model.getValidationEvents(Severity.ERROR).size());
+    }
+
+    @Test
+    public void allowsMixinsOnCollectionShapesWithoutWarningOrError() {
+        ValidatedResult<Model> model = Model.assembler()
+            .addImport(getClass().getResource("mixins/collection-mixins.json"))
+            .assemble();
+        assertEquals(0, model.getValidationEvents(Severity.WARNING).size());
+        assertEquals(0, model.getValidationEvents(Severity.ERROR).size());
     }
 }
