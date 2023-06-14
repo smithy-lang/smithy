9c4bd3efad53ab8351fcba90e6233c954a22cc6a
kstich <kevin@kstich.com>
2023-06-13 20:59:48 -0700

    Extract AWS specific rules engine functions
    This commit pulls the AWS specific rules engine functions into a new
    package, smithy-aws-rules-engine. This includes parseArn, partition,
    and isVirtualHostableS3Bucket. The partition loading has also moved
    to this package, including its data types.
    
    Relevant tests were migrated. Behavior that was only possible to
    validate using the parseArn function have been moved, even if the
    behavior is not AWS specific. Tests that used AWS functions but did
    not require it were updated to be agnostic.

---
+++ smithy-aws-rules-engine/src/test/java/software/amazon/smithy/aws/rulesengine/language/functions/IntegrationTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.smithy.aws.rulesengine.language.functions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import software.amazon.smithy.model.node.Node;
+import software.amazon.smithy.rulesengine.language.EndpointRuleSet;
+import software.amazon.smithy.rulesengine.language.error.RuleError;
+import software.amazon.smithy.utils.IoUtils;
+import software.amazon.smithy.utils.Pair;
+
+public class IntegrationTest {
+    public static List<Pair<Node, String>> invalidRules() throws IOException {
+        try (Stream<Path> paths = Files.list(
+                Paths.get(IntegrationTest.class.getResource("invalid-rules").getPath()))
+        ) {
+            return paths.map(path -> {
+                try {
+                    String pathContents = IoUtils.toUtf8String(new FileInputStream(path.toFile()));
+                    Node content = Node.parseJsonWithComments(pathContents,
+                            path.subpath(path.getNameCount() - 2, path.getNameCount())
+                                    .toString().replace("\\", "/"));
+
+                    List<String> commentLines = new ArrayList<>();
+                    for (String line : pathContents.split(System.lineSeparator())) {
+                        if (line.startsWith("//")) {
+                            commentLines.add(line.substring(3));
+                        }
+                    }
+
+                    return Pair.of(content, String.join("\n", commentLines));
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }).collect(Collectors.toList());
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidRules")
+    public void checkInvalidRules(Pair<Node, String> validationTestCase) {
+        RuleError error = assertThrows(RuleError.class, () -> EndpointRuleSet.fromNode(validationTestCase.getLeft()));
+        assertEquals(validationTestCase.getRight(), error.toString());
+    }
+}
