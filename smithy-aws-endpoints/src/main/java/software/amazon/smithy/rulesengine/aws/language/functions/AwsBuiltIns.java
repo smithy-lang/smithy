11845db1ab895b2b225c79900cf85c31155e3ab4
Alex Woods <alexwoo@amazon.com>
2023-09-29 14:41:48 -0700

    Fix checkstyle, make docs more vague

--- smithy-aws-endpoints/src/main/java/software/amazon/smithy/rulesengine/aws/language/functions/AwsBuiltIns.java
+++ smithy-aws-endpoints/src/main/java/software/amazon/smithy/rulesengine/aws/language/functions/AwsBuiltIns.java
@@ -54,25 +54,25 @@ public final class AwsBuiltIns {
                     .build();
 
     /**
-     * Built-in parameter representing the AccountId
+     * Built-in parameter representing the AccountId.
      */
     public static final Parameter ACCOUNT_ID =
             Parameter.builder()
                     .name("AccountId")
                     .type(ParameterType.STRING)
                     .builtIn("AWS::Auth::AccountId")
-                    .documentation("The AWS AccountId")
+                    .documentation("The AWS AccountId used for the request.")
                     .build();
 
     /**
-     * Built-in parameter representing the AccountId
+     * Built-in parameter representing the Credential Scope.
      */
     public static final Parameter CREDENTIAL_SCOPE =
             Parameter.builder()
                     .name("CredentialScope")
                     .type(ParameterType.STRING)
                     .builtIn("AWS::Auth::CredentialScope")
-                    .documentation("When set, this must be a single valid AWS region (e.g. 'us-east-1').")
+                    .documentation("The AWS Credential Scope used for the request.")
                     .build();
 
     /**
