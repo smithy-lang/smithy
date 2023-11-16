4234d902a93f50b71974cc02bf7f51b59f47ad9c
kstich <kevin@kstich.com>
2023-11-16 09:30:10 -0800

    Fix removal of applied non-prelude meta traits

--- smithy-model/src/main/java/software/amazon/smithy/model/neighbor/UnreferencedTraitDefinitions.java
+++ smithy-model/src/main/java/software/amazon/smithy/model/neighbor/UnreferencedTraitDefinitions.java
@@ -15,6 +15,10 @@
 
 package software.amazon.smithy.model.neighbor;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -23,9 +27,9 @@ import software.amazon.smithy.model.knowledge.NeighborProviderIndex;
 import software.amazon.smithy.model.loader.Prelude;
 import software.amazon.smithy.model.shapes.ServiceShape;
 import software.amazon.smithy.model.shapes.Shape;
+import software.amazon.smithy.model.shapes.ShapeId;
 import software.amazon.smithy.model.traits.TraitDefinition;
 import software.amazon.smithy.utils.FunctionalUtils;
-import software.amazon.smithy.utils.OptionalUtils;
 
 /**
  * Finds trait definitions that are not connected to a service shape.
@@ -64,10 +68,28 @@ public final class UnreferencedTraitDefinitions {
                 .map(Shape::getAllTraits)
                 .flatMap(traits -> traits.keySet().stream())
                 .distinct()
-                .flatMap(traitId -> OptionalUtils.stream(model.getShape(traitId)))
+                .flatMap(traitId -> getTraitShapes(model, traitId).stream())
                 .filter(keepFilter)
                 .forEach(unused::remove);
 
         return unused;
+    }
+
+    private Collection<Shape> getTraitShapes(Model model, ShapeId traitId) {
+        return getTraitShapes(model, traitId, new HashMap<>()).values();
+    }
+
+    private Map<ShapeId, Shape> getTraitShapes(Model model, ShapeId traitId, Map<ShapeId, Shape> traitShapes) {
+        Optional<Shape> initialTraitShapeOp = model.getShape(traitId);
+        if (initialTraitShapeOp.isPresent()) {
+            Shape initialTraitShape = initialTraitShapeOp.get();
+            traitShapes.put(traitId, initialTraitShape);
+            for (ShapeId metaTraitId : initialTraitShape.getAllTraits().keySet()) {
+                if (!metaTraitId.equals(TraitDefinition.ID) && !traitShapes.containsKey(metaTraitId)) {
+                    traitShapes.putAll(getTraitShapes(model, metaTraitId, traitShapes));
+                }
+            }
+        }
+        return traitShapes;
     }
 }
