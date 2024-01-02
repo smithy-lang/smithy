a2e8dcf5f3e25a725fb628674eb318e2d61a5cb0
Richard Chen <rchache@amazon.com>
2024-01-02 16:49:50 -0500

    Changed scalar removal to WARNING

--- smithy-diff/src/main/java/software/amazon/smithy/diff/evaluators/RemovedShape.java
+++ smithy-diff/src/main/java/software/amazon/smithy/diff/evaluators/RemovedShape.java
@@ -19,13 +19,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 import software.amazon.smithy.diff.Differences;
 import software.amazon.smithy.model.shapes.Shape;
+import software.amazon.smithy.model.shapes.ShapeType;
 import software.amazon.smithy.model.traits.EnumTrait;
 import software.amazon.smithy.model.traits.PrivateTrait;
+import software.amazon.smithy.model.validation.Severity;
 import software.amazon.smithy.model.validation.ValidationEvent;
 
 /**
  * Creates an ERROR event when a non-private non-scalar shape is removed.
- * Creates a NOTE event when a non-private scalar shape is removed.
+ * Creates a WARNING event when a non-private scalar shape is removed.
  */
 public final class RemovedShape extends AbstractDiffEvaluator {
     @Override
@@ -33,8 +35,14 @@ public final class RemovedShape extends AbstractDiffEvaluator {
         return differences.removedShapes()
                 .filter(shape -> !shape.hasTrait(PrivateTrait.class))
                 .filter(shape -> !isMemberOfRemovedShape(shape, differences))
-                .map(shape -> isScalarType(shape)
-                        ? note(shape, String.format("Removed %s `%s`", shape.getType(), shape.getId()))
+                .map(shape -> isInconsequentialType(shape)
+                        ? ValidationEvent.builder()
+                            .severity(Severity.WARNING)
+                            .message(String.format("Removed %s `%s`", shape.getType(), shape.getId()))
+                            .shapeId(shape.getId())
+                            .id(getEventId() + ".ScalarShape")
+                            .sourceLocation(shape.getSourceLocation())
+                            .build()
                         : error(shape, String.format("Removed %s `%s`", shape.getType(), shape.getId())))
                 .collect(Collectors.toList());
     }
@@ -45,18 +53,19 @@ public final class RemovedShape extends AbstractDiffEvaluator {
                 .isPresent();
     }
 
-    private boolean isScalarType(Shape shape) {
-        return shape.isBigDecimalShape()
-            || shape.isBigIntegerShape()
-            || shape.isBlobShape()
-            || shape.isBooleanShape()
-            || shape.isByteShape()
-            || shape.isDoubleShape()
-            || shape.isFloatShape()
-            || shape.isShortShape()
-            || shape.isTimestampShape()
-            || shape.isLongShape()
-            || (shape.isStringShape() && !shape.hasTrait(EnumTrait.class))
-            || (shape.isIntegerShape() && !shape.isIntEnumShape());
+    private boolean isInconsequentialType(Shape shape) {
+        ShapeType shapeType = shape.getType();
+        return shapeType == ShapeType.BIG_DECIMAL
+                || shapeType == ShapeType.BIG_INTEGER
+                || shapeType == ShapeType.BLOB
+                || shapeType == ShapeType.BOOLEAN
+                || shapeType == ShapeType.BYTE
+                || shapeType == ShapeType.DOUBLE
+                || shapeType == ShapeType.FLOAT
+                || shapeType == ShapeType.SHORT
+                || shapeType == ShapeType.TIMESTAMP
+                || shapeType == ShapeType.LONG
+                || ((shapeType == ShapeType.STRING) && !shape.hasTrait(EnumTrait.class))
+                || (shapeType == ShapeType.INTEGER);
     }
 }
