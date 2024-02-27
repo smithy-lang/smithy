7b10d93718e60125d7cae4470fbbc48b1f4b3ef9
Hunter Mellema <hpm@amazon.com>
2024-02-27 15:29:14 -0700

    Remove mapper class

--- smithy-trait-codegen/src/main/java/software/amazon/smithy/traitcodegen/SymbolProperties.java
+++ smithy-trait-codegen/src/main/java/software/amazon/smithy/traitcodegen/SymbolProperties.java
@@ -9,11 +9,13 @@ import software.amazon.smithy.utils.SmithyInternalApi;
 
 @SmithyInternalApi
 public final class SymbolProperties {
-    // Provides an initializer for the builder ref
+    // Provides an initializer for the builder ref. This should always
+    // be included on collection shapes such as Maps and Lists
     public static final String BUILDER_REF_INITIALIZER = "builder-ref-initializer";
 
     // The "base" symbol for a trait. This is the symbol that the shape would resolve
-    // to if it were not marked with `@trait`.
+    // to if it were not marked with `@trait`. This property is expected on all
+    // trait symbols.
     public static final String BASE_SYMBOL = "base-symbol";
 
     private SymbolProperties() {
