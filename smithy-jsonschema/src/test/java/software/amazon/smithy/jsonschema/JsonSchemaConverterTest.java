8e9ec7da5cbf8cb651159ed815adf4c7ec511a28
Miguel Vilá <miguelvilag@gmail.com>
2024-04-11 10:26:48 +0100

    reformat
    Co-authored-by: Kevin Stich <kevin@kstich.com>

--- smithy-jsonschema/src/test/java/software/amazon/smithy/jsonschema/JsonSchemaConverterTest.java
+++ smithy-jsonschema/src/test/java/software/amazon/smithy/jsonschema/JsonSchemaConverterTest.java
@@ -865,14 +865,16 @@ public class JsonSchemaConverterTest {
     public void dontAddDeprecatedTraitOnAMemberWhenOldVersion() {
         StringShape string = StringShape.builder().id("smithy.api#String").build();
         StructureShape shape = StructureShape.builder()
-        .id(ShapeId.from("a.b#C"))
-        .addMember(
-                MemberShape.builder().id(ShapeId.from("a.b#C$member"))
-                .target(string.getId())
-                .addTrait(DeprecatedTrait.builder().message(
-                        "I'm deprecated"
-                ).since("sinceVersion").build()).build()
-        ).build();
+                .id(ShapeId.from("a.b#C"))
+                .addMember(MemberShape.builder()
+                        .id(ShapeId.from("a.b#C$member"))
+                        .target(string.getId())
+                        .addTrait(DeprecatedTrait.builder()
+                                .message("I'm deprecated")
+                                .since("sinceVersion")
+                                .build())
+                        .build())
+                .build();
         Model model = Model.builder().addShapes(shape, string).build();
         JsonSchemaConfig config = new JsonSchemaConfig();
         config.setJsonSchemaVersion(JsonSchemaVersion.DRAFT07);
