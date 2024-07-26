31a64983de26f3e5408ae38dfea288d64ac980b2
maximk000 <57971916+maximk000@users.noreply.github.com>
2024-07-25 17:04:53 -0700

    Add intEnum validation to NodeValidationVisitor (#2357)
    * Add intEnum validation to NodeValidationVisitor
    
    * Use new copyright notice
    
    Co-authored-by: Manuel Sugawara <sugmanue@amazon.com>
    
    ---------
    
    Co-authored-by: Maxim Korolkov <mkorolko@amazon.com>
    Co-authored-by: Manuel Sugawara <sugmanue@amazon.com>

---
+++ smithy-model/src/main/java/software/amazon/smithy/model/validation/node/IntEnumPlugin.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.smithy.model.validation.node;
+
+import java.util.Collection;
+import software.amazon.smithy.model.node.NumberNode;
+import software.amazon.smithy.model.shapes.IntEnumShape;
+import software.amazon.smithy.model.validation.ValidationUtils;
+
+
+/**
+ * Validates NumberNodes against intEnum shapes' allowed enum values.
+ */
+final class IntEnumPlugin extends FilteredPlugin<IntEnumShape, NumberNode> {
+
+    IntEnumPlugin() {
+        super(IntEnumShape.class, NumberNode.class);
+    }
+
+    @Override
+    protected void check(IntEnumShape shape, NumberNode node, Context context, Emitter emitter) {
+        Collection<Integer> values = shape.getEnumValues().values();
+        if (!values.contains(node.getValue().intValue())) {
+            emitter.accept(node, String.format(
+                    "Integer value provided for `%s` must be one of the following values: %s, but found %s",
+                    shape.getId(), ValidationUtils.tickedList(values), node.getValue()));
+        }
+    }
+}
