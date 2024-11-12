857f05d23de077a794b93bf217a2c4ac34906933
Miles Ziemer <45497130+milesziemer@users.noreply.github.com>
2024-11-12 10:37:24 -0500

    Add validator for xmlFlattened + xmlName (#2439)
    Adds a validator for the xmlFlattened trait that checks if the member's
    target is a list that has a member with xmlName, and that xmlName
    doesn't match the name of the xmlFlattened member. xmlFlattened causes
    the xmlName of the list member to be ignored. This is called out in our
    docs:
    https://smithy.io/2.0/spec/protocol-traits.html#flattened-list-serialization,
    but we were missing a validator for it. It is a warning because you might
    mean to do this, i.e. if the list is used in multiple places.
    
    I also added some suppressions to our protocol tests that are testing
    the behavior for models that would trip up this validator.

---
+++ smithy-model/src/main/java/software/amazon/smithy/model/validation/validators/XmlFlattenedTraitValidator.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.smithy.model.validation.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.shapes.ListShape;
+import software.amazon.smithy.model.shapes.MemberShape;
+import software.amazon.smithy.model.shapes.Shape;
+import software.amazon.smithy.model.traits.XmlFlattenedTrait;
+import software.amazon.smithy.model.traits.XmlNameTrait;
+import software.amazon.smithy.model.validation.AbstractValidator;
+import software.amazon.smithy.model.validation.ValidationEvent;
+
+/**
+ * Validates that xmlFlattened members aren't unintentionally ignoring the
+ * xmlName of their targets.
+ */
+public final class XmlFlattenedTraitValidator extends AbstractValidator {
+    @Override
+    public List<ValidationEvent> validate(Model model) {
+        List<ValidationEvent> events = new ArrayList<>();
+        for (MemberShape member : model.getMemberShapesWithTrait(XmlFlattenedTrait.class)) {
+            // Don't emit the event if they're being explicit about the xmlName on this member
+            if (member.hasTrait(XmlNameTrait.class)) {
+                continue;
+            }
+
+            Shape target = model.expectShape(member.getTarget());
+            if (target instanceof ListShape) {
+                ListShape targetList = (ListShape) target;
+                validateMemberTargetingList(member, targetList, events);
+            }
+        }
+        return events;
+    }
+
+    private void validateMemberTargetingList(MemberShape member, ListShape targetList, List<ValidationEvent> events) {
+        if (targetList.getMember().hasTrait(XmlNameTrait.class)) {
+            XmlNameTrait xmlName = targetList.getMember().expectTrait(XmlNameTrait.class);
+            if (!member.getMemberName().equals(xmlName.getValue())) {
+                events.add(warning(member, String.format(
+                        "Member is `@xmlFlattened`, so `@xmlName` of target's member (`%s`) has no effect."
+                        + " The flattened list elements will have the name of this member - `%s`. If this"
+                        + " is unintended, you can add `@xmlName(\"%s\")` to this member.",
+                        targetList.getMember().getId(), member.getMemberName(), xmlName.getValue())));
+            }
+        }
+    }
+}
