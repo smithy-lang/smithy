559eec155ae4308945c84dd6fb2bedd31b566cd9
JordonPhillips <JordonPhillips@users.noreply.github.com>
2025-02-26 17:27:21 +0100

    Bump Spotless to v7
    This updates spotless to v7
    
    Kotlin lambdas can't be serialized effectively by {spotless / gradle}
    right now so this replaces those lambdas with anonymous classes that
    implement `FormatterFunc`

--- buildSrc/src/main/kotlin/smithy.formatting-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/smithy.formatting-conventions.gradle.kts
@@ -1,3 +1,5 @@
+import com.diffplug.spotless.FormatterFunc
+import java.io.Serializable
 import java.util.regex.Pattern
 
 plugins {
@@ -14,13 +16,25 @@ spotless {
         // Enforce a common license header on all files
         licenseHeaderFile("${project.rootDir}/config/spotless/license-header.txt")
             .onlyIfContentMatches("^((?!SKIPLICENSECHECK)[\\s\\S])*\$")
-        indentWithSpaces()
+        leadingTabsToSpaces()
         endWithNewline()
         eclipse().configFile("${project.rootDir}/config/spotless/formatting.xml")
+
         // Fixes for some strange formatting applied by eclipse:
         // see: https://github.com/kamkie/demo-spring-jsf/blob/bcacb9dc90273a5f8d2569470c5bf67b171c7d62/build.gradle.kts#L159
-        custom("Lambda fix") { it.replace("} )", "})").replace("} ,", "},") }
-        custom("Long literal fix") { Pattern.compile("([0-9_]+) [Ll]").matcher(it).replaceAll("\$1L") }
+        // These have to be implemented with anonymous classes this way instead of lambdas because of:
+        // https://github.com/diffplug/spotless/issues/2387
+        custom("Lambda fix", object : Serializable, FormatterFunc {
+            override fun apply(input: String) : String {
+                return input.replace("} )", "})").replace("} ,", "},")
+            }
+        })
+        custom("Long literal fix", object : Serializable, FormatterFunc {
+            override fun apply(input: String) : String {
+                return Pattern.compile("([0-9_]+) [Ll]").matcher(input).replaceAll("\$1L")
+            }
+        })
+
         // Static first, then everything else alphabetically
         removeUnusedImports()
         importOrder("\\#", "")
@@ -31,7 +45,7 @@ spotless {
     // Formatting for build.gradle.kts files
     kotlinGradle {
         ktlint()
-        indentWithSpaces()
+        leadingTabsToSpaces()
         trimTrailingWhitespace()
         endWithNewline()
         licenseHeaderFile(
