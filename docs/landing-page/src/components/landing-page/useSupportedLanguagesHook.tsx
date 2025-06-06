fc5cb31abcc3417f2ecfa4784b60ae772467903f
Joe Wu <yizhouwu@amazon.com>
2025-06-06 15:37:47 -0700

    Improve Smithy landing page and documentation page
    Improvement list:
    - Update "Ecosystem" button to "Awesome Smithy" and its link
    - Add hover tooltip for client and server circles
    - Add max width for heading and sub-heading blocks
    - Move "Quickstart" button from top navigation bar to heading block and rename "Get Started"
    - Set Smithy documentation sidebar logo to redirect to current Smithy landing page

--- docs/landing-page/src/components/landing-page/useSupportedLanguagesHook.tsx
+++ docs/landing-page/src/components/landing-page/useSupportedLanguagesHook.tsx
@@ -13,18 +13,21 @@ export const useSupportedLanguages = (props?: useSupportedLanguagesProps) => {
     const serverUrls = [
       {
         src: "/icons/duke.svg",
+        name: "Java",
         alt: t("java.alt"),
         trademark: t("java.trademark"),
         location: "https://github.com/smithy-lang/smithy-java",
       },
       {
         src: "/icons/ts.svg",
+        name: "TypeScript",
         alt: t("ts.alt"),
         trademark: t("ts.trademark"),
-        location: "https://github.com/awslabs/smithy-typescript",
+        location: "https://github.com/smithy-lang/smithy-typescript",
       },
       {
         src: "/icons/rust.svg",
+        name: "Rust",
         alt: t("rust.alt"),
         trademark: (
           <>
@@ -42,6 +45,7 @@ export const useSupportedLanguages = (props?: useSupportedLanguagesProps) => {
       },
       {
         src: "/icons/scala.svg",
+        name: "Scala",
         alt: t("scala.alt"),
         trademark: t("scala.trademark"),
         location: "https://github.com/disneystreaming/smithy4s",
@@ -50,30 +54,35 @@ export const useSupportedLanguages = (props?: useSupportedLanguagesProps) => {
     const clientUrls = [
       {
         src: "/icons/python.svg",
+        name: "Python",
         alt: t("python.alt"),
         trademark: t("python.trademark"),
         location: "https://github.com/smithy-lang/smithy-python",
       },
       {
         src: "/icons/go.svg",
+        name: "Go",
         alt: t("go.alt"),
         trademark: t("go.trademark"),
         location: "https://github.com/aws/smithy-go",
       },
       {
         src: "/icons/kotlin.svg",
+        name: "Kotlin",
         alt: t("kotlin.alt"),
         trademark: t("kotlin.trademark"),
         location: "https://github.com/awslabs/smithy-kotlin",
       },
       {
         src: "/icons/swift.svg",
+        name: "Swift",
         alt: t("swift.alt"),
         trademark: t("swift.trademark"),
         location: "https://github.com/awslabs/smithy-swift",
       },
       {
         src: "/icons/ruby.png",
+        name: "Ruby",
         alt: t("ruby.alt"),
         trademark: t("ruby.trademark"),
         location: "https://github.com/awslabs/smithy-ruby",
