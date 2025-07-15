2801b8c046c297251b796ec7de7f999fc593bb66
JordonPhillips <JordonPhillips@users.noreply.github.com>
2025-07-15 16:23:14 +0200

    Move changelog CLI scripts to package
    This moves the changelog CLI scripts into the package itself. This
    allows them to be used in the `pyproject`'s `scripts` table, which
    then lets it be used by uv and other python tools. It also will make
    it easier to share these later if we need to.

--- .changes/tool/render.py
+++ .changes/tool/render.py
@@ -1,8 +1,49 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
+from argparse import ArgumentParser
+
 from . import RELEASES_DIR, Change, Release
+from .release import release as _release
 
 TITLE = "Changelog"
+
+
+def main() -> None:
+    parser = ArgumentParser(
+        description="""\
+            Render the changelog as markdown, optionally including pending features \
+            as a new release.""",
+    )
+    release_group = parser.add_argument_group(
+        "release",
+        description="""\
+            These arguments allow for releasing all pending features in the \
+            next-release folder as a new release. If not set, the exisiting releases \
+            will be re-rendered.""",
+    )
+    release_group.add_argument(
+        "-v",
+        "--release-version",
+        type=str,
+        help="""\
+            The version to use for the staged changelog release. If set, all pending \
+            features will be compiled into a release.""",
+    )
+    release_group.add_argument(
+        "-d",
+        "--release-date",
+        type=str,
+        help="""\
+            The date of the release in ISO format (e.g. 2024-11-13). If not set, \
+            today's date, according to your local time zone, will be used.""",
+    )
+
+    args = parser.parse_args()
+
+    if args.release_version:
+        _release(args.release_version, args.release_date)
+
+    render()
 
 
 def render() -> None:
