dd7703d89d63448f0ff913459799124fe66af53c
JordonPhillips <JordonPhillips@users.noreply.github.com>
2025-09-10 22:29:19 +0200

    Use DependencyGraph in integration sorting
    This updates the SmithyIntegration sorting to use the generic
    DependencyGraph. It also adds some benchmarking configurations,
    which show a minor speed bump for highly dependent integrations
    and a major speed bump for highly independent integrations.

--- smithy-codegen-core/src/main/java/software/amazon/smithy/codegen/core/IntegrationTopologicalSort.java
+++ smithy-codegen-core/src/main/java/software/amazon/smithy/codegen/core/IntegrationTopologicalSort.java
@@ -5,16 +5,12 @@
 package software.amazon.smithy.codegen.core;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
 import java.util.logging.Logger;
+import software.amazon.smithy.utils.DependencyGraph;
 
 final class IntegrationTopologicalSort<I extends SmithyIntegration<?, ?, ?>> {
 
@@ -22,12 +18,54 @@ final class IntegrationTopologicalSort<I extends SmithyIntegration<?, ?, ?>> {
 
     private final Map<String, I> integrationLookup = new LinkedHashMap<>();
     private final Map<String, Integer> insertionOrder = new HashMap<>();
-    private final Map<String, Set<String>> forwardDependencies = new LinkedHashMap<>();
-    private final Map<String, Set<String>> reverseDependencies = new LinkedHashMap<>();
+    private final DependencyGraph<String> dependencyGraph = new DependencyGraph<>();
 
-    private final Queue<String> satisfied = new PriorityQueue<>((left, right) -> {
+    IntegrationTopologicalSort(Iterable<I> integrations) {
+        for (I integration : integrations) {
+            I previous = this.integrationLookup.put(integration.name(), integration);
+            if (previous != null) {
+                throw new IllegalArgumentException(String.format(
+                        "Conflicting SmithyIntegration names detected for '%s': %s and %s",
+                        integration.name(),
+                        integration.getClass().getCanonicalName(),
+                        previous.getClass().getCanonicalName()));
+            }
+            insertionOrder.put(integration.name(), insertionOrder.size());
+            dependencyGraph.add(integration.name());
+        }
+        for (I integration : integrations) {
+            dependencyGraph.addDependencies(integration.name(), integration.runAfter());
+            for (String dependant : integration.runBefore()) {
+                dependencyGraph.addDependency(dependant, integration.name());
+            }
+        }
+    }
+
+    List<I> sort() {
+        List<I> result = new ArrayList<>(dependencyGraph.size());
+        List<String> sorted;
+        try {
+            sorted = dependencyGraph.toSortedList(this::compareIntegrations);
+        } catch (IllegalStateException e) {
+            throw new IllegalArgumentException(e);
+        }
+        for (String name : sorted) {
+            I integration = integrationLookup.get(name);
+            if (integration != null) {
+                result.add(integration);
+            } else {
+                logMissingIntegration(name);
+            }
+        }
+        return result;
+    }
+
+    private int compareIntegrations(String left, String right) {
         I leftIntegration = integrationLookup.get(left);
         I rightIntegration = integrationLookup.get(right);
+        if (leftIntegration == null || rightIntegration == null) {
+            return 0;
+        }
         // Priority order is used to sort first.
         int byteResult = Byte.compare(rightIntegration.priority(), leftIntegration.priority());
         // If priority is a tie, then sort based on insertion order of integrations.
@@ -35,89 +73,26 @@ final class IntegrationTopologicalSort<I extends SmithyIntegration<?, ?, ?>> {
         return byteResult == 0
                 ? Integer.compare(insertionOrder.get(left), insertionOrder.get(right))
                 : byteResult;
-    });
-
-    IntegrationTopologicalSort(Iterable<I> integrations) {
-        // Validate name conflicts and register integrations with the lookup table + insertion order table.
-        for (I integration : integrations) {
-            addIntegration(integration);
-        }
-
-        // Validate missing dependencies and add found dependencies.
-        for (I integration : integrations) {
-            for (String before : getValidatedDependencies("before", integration.name(), integration.runBefore())) {
-                addDependency(before, integration.name());
-            }
-            for (String after : getValidatedDependencies("after", integration.name(), integration.runAfter())) {
-                addDependency(integration.name(), after);
-            }
-        }
-
-        // Offer satisfied dependencies.
-        for (I integration : integrations) {
-            if (!forwardDependencies.containsKey(integration.name())) {
-                satisfied.offer(integration.name());
-            }
-        }
     }
 
-    private void addIntegration(I integration) {
-        I previous = this.integrationLookup.put(integration.name(), integration);
-        insertionOrder.put(integration.name(), insertionOrder.size());
-        if (previous != null) {
-            throw new IllegalArgumentException(String.format(
-                    "Conflicting SmithyIntegration names detected for '%s': %s and %s",
-                    integration.name(),
-                    integration.getClass().getCanonicalName(),
-                    previous.getClass().getCanonicalName()));
+    private void logMissingIntegration(String name) {
+        StringBuilder message = new StringBuilder("Could not find SmithyIntegration named '");
+        message.append(name).append('\'');
+        if (!dependencyGraph.getDirectDependants(name).isEmpty()) {
+            message.append(" that was supposed to run before integrations [");
+            message.append(String.join(", ", dependencyGraph.getDirectDependants(name)));
+            message.append("]");
         }
-    }
-
-    private List<String> getValidatedDependencies(String descriptor, String what, List<String> dependencies) {
-        if (dependencies.isEmpty()) {
-            return dependencies;
-        } else {
-            List<String> filtered = new ArrayList<>(dependencies);
-            filtered.removeIf(value -> {
-                if (integrationLookup.containsKey(value)) {
-                    return false;
-                } else {
-                    LOGGER.warning(what + " is supposed to run " + descriptor + " an integration that could "
-                            + "not be found, '" + value + "'");
-                    return true;
-                }
-            });
-            return filtered;
-        }
-    }
-
-    private void addDependency(String what, String dependsOn) {
-        forwardDependencies.computeIfAbsent(what, n -> new LinkedHashSet<>()).add(dependsOn);
-        reverseDependencies.computeIfAbsent(dependsOn, n -> new LinkedHashSet<>()).add(what);
-    }
-
-    List<I> sort() {
-        List<I> result = new ArrayList<>();
-
-        while (!satisfied.isEmpty()) {
-            String current = satisfied.poll();
-            forwardDependencies.remove(current);
-            result.add(integrationLookup.get(current));
-
-            for (String dependent : reverseDependencies.getOrDefault(current, Collections.emptySet())) {
-                Set<String> dependentDependencies = forwardDependencies.get(dependent);
-                dependentDependencies.remove(current);
-                if (dependentDependencies.isEmpty()) {
-                    satisfied.offer(dependent);
-                }
+        if (!dependencyGraph.getDirectDependencies(name).isEmpty()) {
+            if (!dependencyGraph.getDirectDependants(name).isEmpty()) {
+                message.append(" and ");
+            } else {
+                message.append(" that ");
             }
+            message.append("was supposed to run after integrations [");
+            message.append(String.join(", ", dependencyGraph.getDirectDependencies(name)));
+            message.append("]");
         }
-
-        if (!forwardDependencies.isEmpty()) {
-            throw new IllegalArgumentException("SmithyIntegration cycles detected among "
-                    + forwardDependencies.keySet());
-        }
-
-        return result;
+        LOGGER.warning(message.toString());
     }
 }
