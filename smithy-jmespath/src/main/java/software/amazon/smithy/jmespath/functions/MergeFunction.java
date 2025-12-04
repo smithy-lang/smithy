7c408fb457c51e59a9d1a82e20550b57359c725a
Robin Salkeld <salkeldr@amazon.com>
2025-12-04 13:40:59 -0800

    All functions

---
+++ smithy-jmespath/src/main/java/software/amazon/smithy/jmespath/functions/MergeFunction.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package software.amazon.smithy.jmespath.functions;
+
+import java.util.List;
+import software.amazon.smithy.jmespath.evaluation.JmespathRuntime;
+
+public class MergeFunction implements Function {
+    @Override
+    public String name() {
+        return "merge";
+    }
+
+    @Override
+    public <T> T apply(JmespathRuntime<T> runtime, List<FunctionArgument<T>> functionArguments) {
+        JmespathRuntime.ObjectBuilder<T> builder = runtime.objectBuilder();
+        
+        for (FunctionArgument<T> arg : functionArguments) {
+            T object = arg.expectObject();
+            builder.putAll(object);
+        }
+        
+        return builder.build();
+    }
+}
