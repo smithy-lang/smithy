943dfaf13d47c1cbdbb2af689bedb1e5f61e87cc
Robin Salkeld <salkeldr@amazon.com>
2025-12-17 21:29:27 -0800

    PR feedback

--- smithy-jmespath/src/main/java/software/amazon/smithy/jmespath/JmespathExceptionType.java
+++ smithy-jmespath/src/main/java/software/amazon/smithy/jmespath/JmespathExceptionType.java
@@ -4,20 +4,47 @@
  */
 package software.amazon.smithy.jmespath;
 
+/**
+ * Defines categories of JMESPath errors.
+ * A superset of the types defined in <a href="https://jmespath.org/specification.html#errors">the specification.
+ */
 public enum JmespathExceptionType {
+    /**
+     * The "syntax" class of JMESPath errors.
+     * <p>
+     * "syntax" is not listed in the specification, but it is used
+     * in <a href="https://github.com/jmespath/jmespath.test">the compliance tests</a> to indicate invalid expressions.
+     */
     SYNTAX,
+
+    /**
+     * The "invalid-type" class of JMESPath errors.
+     */
     INVALID_TYPE,
+
+    /**
+     * The "invalid-value" class of JMESPath errors.
+     */
     INVALID_VALUE,
+
+    /**
+     * The "unknown-function" class of JMESPath errors.
+     */
     UNKNOWN_FUNCTION,
+
+    /**
+     * The "invalid-arity" class of JMESPath errors.
+     */
     INVALID_ARITY,
+
+    /**
+     * Any other error cause.
+     */
     OTHER;
 
     /**
      * Returns the corresponding enum value for one of the identifiers used in
      * <a href="https://jmespath.org/specification.html#errors">the JMESPath specification</a>.
-     * <p>
-     * "syntax" is not listed in the specification, but it is used
-     * in <a href="https://github.com/jmespath/jmespath.test">the compliance tests</a> to indicate invalid expressions.
      */
     public static JmespathExceptionType fromID(String id) {
         return valueOf(id.toUpperCase().replace('-', '_'));
