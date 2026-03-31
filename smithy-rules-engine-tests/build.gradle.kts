d66936a4bbd0e6d2856185fb0c3f2e6f93cb472f
Joe Wu <yizhouwu@amazon.com>
2026-03-31 13:46:26 -0700

    Add BDD auto generatrion in rules-engine-tests (#3027)
    * Add BDD auto generatrion in rules-engine-tests
    
    * Add changelog
    
    * Change projection name

--- smithy-rules-engine-tests/build.gradle.kts
+++ smithy-rules-engine-tests/build.gradle.kts
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 plugins {
+    alias(libs.plugins.smithy.jar)
     id("smithy.module-conventions")
 }
 
@@ -12,7 +13,17 @@ extra["displayName"] = "Smithy :: Rules Engine :: Tests"
 extra["moduleName"] = "software.amazon.smithy.rulesenginetests"
 
 dependencies {
+    implementation(project(path = ":smithy-cli", configuration = "shadow"))
     api(project(":smithy-rules-engine"))
     api(project(":smithy-model"))
     api(project(":smithy-utils"))
+}
+
+tasks.sourcesJar {
+    dependsOn("smithyJarStaging")
+}
+
+smithy {
+    format.set(false)
+    sourceProjection.set("bddSource")
 }
