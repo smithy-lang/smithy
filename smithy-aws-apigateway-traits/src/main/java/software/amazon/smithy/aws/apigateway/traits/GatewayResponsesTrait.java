da96cfd301940d4eff396db8fe7aa95757c36668
Meena <49625228+mee-aa@users.noreply.github.com>
2026-05-06 17:03:03 -0700

    Update smithy-aws-apigateway-traits/src/main/java/software/amazon/smithy/aws/apigateway/traits/GatewayResponsesTrait.java
    Co-authored-by: Manuel Sugawara <sugmanue@amazon.com>

--- smithy-aws-apigateway-traits/src/main/java/software/amazon/smithy/aws/apigateway/traits/GatewayResponsesTrait.java
+++ smithy-aws-apigateway-traits/src/main/java/software/amazon/smithy/aws/apigateway/traits/GatewayResponsesTrait.java
@@ -44,24 +44,6 @@ public final class GatewayResponsesTrait extends AbstractTrait implements ToSmit
         return new Builder();
     }
 
-    public static final class Provider extends AbstractTrait.Provider {
-        public Provider() {
-            super(ID);
-        }
-
-        @Override
-        public Trait createTrait(ShapeId target, Node value) {
-            Builder builder = builder().sourceLocation(value);
-            for (Map.Entry<StringNode, Node> entry : value.expectObjectNode().getMembers().entrySet()) {
-                GatewayResponse response = GatewayResponse.fromNode(entry.getValue());
-                builder.putResponse(entry.getKey().getValue(), response);
-            }
-            GatewayResponsesTrait result = builder.build();
-            result.setNodeCache(value);
-            return result;
-        }
-    }
-
     /**
      * Gets all gateway responses.
      *
