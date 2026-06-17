870af0d7871c325860eddd98ff095c808ffea004
Michael Dowling <mtdowling@gmail.com>
2026-06-17 12:49:21 -0500

    Optimize attribute selectors

--- smithy-model/src/main/java/software/amazon/smithy/model/selector/TraitExistenceSelector.java
+++ smithy-model/src/main/java/software/amazon/smithy/model/selector/TraitExistenceSelector.java
@@ -5,11 +5,9 @@
 package software.amazon.smithy.model.selector;
 
 import java.util.Collection;
-import java.util.List;
 import software.amazon.smithy.model.Model;
 import software.amazon.smithy.model.shapes.Shape;
 import software.amazon.smithy.model.shapes.ShapeId;
-import software.amazon.smithy.model.traits.Trait;
 
 /**
  * Specialized selector for the extremely common {@code [trait|X]} existence check.
@@ -22,29 +20,11 @@ final class TraitExistenceSelector implements InternalSelector {
         this.traitId = traitId;
     }
 
-    /**
-     * Creates a TraitExistenceSelector if the path represents a simple {@code [trait|X]} existence check,
-     * otherwise returns null.
-     *
-     * @param path The attribute path parsed from the selector.
-     * @return A TraitExistenceSelector if applicable, otherwise null.
-     */
-    static TraitExistenceSelector tryCreate(List<String> path) {
-        if (path.size() == 2
-                && path.get(0).equals("trait")
-                && !path.get(1).startsWith("(")) {
-            ShapeId traitId = ShapeId.from(Trait.makeAbsoluteName(path.get(1)));
-            return new TraitExistenceSelector(traitId);
-        }
-        return null;
-    }
-
     @Override
     public Response push(Context context, Shape shape, Receiver next) {
-        if (shape.hasTrait(traitId)) {
-            return next.apply(context, shape);
-        }
-        return Response.CONTINUE;
+        return shape.hasTrait(traitId)
+                ? next.apply(context, shape)
+                : Response.CONTINUE;
     }
 
     @Override
